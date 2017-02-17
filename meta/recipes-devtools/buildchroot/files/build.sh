2d56fc1242ef3e3041eb248942324eb67e9b1319
Frank Lenormand <lenormf@gmail.com>
2017-02-17 17:04:38 +0100

    build.sh: Handle arch in Build-Depends
    Handle the "package [arch]" syntax in Debian's "Build-Depends" field.
    
    Signed-off-by: Frank Lenormand <lenormf@gmail.com>
    Signed-off-by: Baurzhan Ismagulov <ibr@ilbers.de>

--- meta/recipes-devtools/buildchroot/files/build.sh
+++ meta/recipes-devtools/buildchroot/files/build.sh
@@ -9,7 +9,7 @@ cd $1
 # Get list of dependencies manually. The package is not in apt, so no apt-get
 # build-dep. dpkg-checkbuilddeps output contains version information and isn't
 # directly suitable for apt-get install.
-DEPS=`perl -ne 'next if /^#/; $p=(s/^Build-Depends:\s*/ / or (/^ / and $p)); s/,|\n|\([^)]+\)//mg; print if $p' < debian/control`
+DEPS=`perl -ne 'next if /^#/; $p=(s/^Build-Depends:\s*/ / or (/^ / and $p)); s/,|\n|\([^)]+\)|\[[^]]+\]//mg; print if $p' < debian/control`
 
 # Install deps
 apt-get install -y $DEPS
