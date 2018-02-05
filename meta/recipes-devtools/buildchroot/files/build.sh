ilbers/isar

commit 498b128ffaf7a0261b216865150a7361c6082371

build.sh: Yet another fix for the installation of build deps
The perl code guessing the build-deps of packages can not handle all the
possible syntax that could be found in Build-Depends. And it ignores
Build-Depends-Indep. So there are several issues in it.

This patch uses an official debian-tool "mk-build-deb" to install the build
deps. The tool generates a package which which will Depends: on the
Build-Depends:. Architecture operators, -Indep etc. are handled
correctly.

Signed-off-by: Henning Schild <henning.schild@siemens.com>

meta/recipes-devtools/buildchroot/files/build.sh
@@ -6,13 +6,8 @@
 # Go to build directory
 cd $1
 
-# Get list of dependencies manually. The package is not in apt, so no apt-get
-# build-dep. dpkg-checkbuilddeps output contains version information and isn't
-# directly suitable for apt-get install.
-DEPS=`perl -ne 'next if /^#/; $p=(s/^Build-Depends:\s*/ / or (/^ / and $p)); s/,|\n|\([^)]+\)|\[[^]]+\]//mg; print if $p' < debian/control`
-
-# Install deps
-apt-get install -y $DEPS
+# Install all build deps
+mk-build-deps -i -r debian/control
 
 # If autotools files have been created, update their timestamp to
 # prevent them from being regenerated

meta/recipes-devtools/buildchroot/buildchroot.bb
@@ -25,7 +25,9 @@ BUILDCHROOT_PREINSTALL ?= "gcc \
                            locales \
                            docbook-to-man \
                            apt \
-                           automake"
+                           automake \
+                           devscripts \
+                           equivs"
 
 WORKDIR = "${TMPDIR}/work/${DISTRO}-${DISTRO_ARCH}/${PN}"
 

commit 6ba1465c4132ba9a5801b8af9f4e005f9380422d

build.sh: eliminate potential prompts from mk-build-deps/apt-get
commit 498b128 caused build dependencies to be installed via mk-build-deps.

The tool may ask the user to confirm installation of the dependencies but
would fail when called from bitbake (no stdin). Override the default install
command to add the -y switch to the apt-get command it uses.

Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

meta/recipes-devtools/buildchroot/files/build.sh
@@ -6,8 +6,14 @@
 # Go to build directory
 cd $1
 
+# Install command to be used by mk-build-deps
+# Notes:
+#   1) everything before the -y switch is unchanged from the defaults
+#   2) we add -y to go non-interactive
+install_cmd="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"
+
 # Install all build deps
-mk-build-deps -i -r debian/control
+mk-build-deps -t "${install_cmd}" -i -r debian/control
 
 # If autotools files have been created, update their timestamp to
 # prevent them from being regenerated