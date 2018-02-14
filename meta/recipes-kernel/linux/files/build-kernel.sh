04e5fa5a5ee03bb4e208e4f80a8a32dddf93d439
Jan Kiszka <jan.kiszka@siemens.com>
2018-02-14 10:13:12 +0100

    Provide include for easy custom kernel builds
    With this include, it becomes almost trivial to replace the distro
    kernel with a custom build. You just need to include linux-custom.inc,
    specify the source URI, define via S where the source is unpacked to and
    provide a defconfig.
    
    To switch to a custom kernel recipe, KERNEL_NAME has to be adjusted in
    local.conf or the machine configuration.
    
    Unless explicitly stated via KERNEL_FLAVOR, this tag is derived from
    the including recipe name which then needs to follow the scheme
    "linux-<flavor>[_version].bb".
    
    The different kernel defconfig file name can be specified via the
    KERNEL_DEFCONFIG variable so that also machine-specific defconfigs can
    be set in the same recipe (KERNEL_DEFCONFIG_my-machine = "...").
    
    The approach works internally by first running "make deb-pkg" on the
    kernel and the repackages the output to make the binary linux-image and
    linux-header debs act as proper replacement of a distro kernel. This
    results in a suboptimal technical implementation which may eventually be
    replaced by an isar-implemented deb-pkg build process. However, this is
    not expected to affect the user-visible interface of this class.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ meta/recipes-kernel/linux/files/build-kernel.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+set -e
+
+REPACK_DIR="$1/../repack"
+REPACK_LINUX_IMAGE_DIR="${REPACK_DIR}/linux-image"
+REPACK_LINUX_HEADERS_DIR="${REPACK_DIR}/linux-headers"
+
+apt-get install -y -o Debug::pkgProblemResolver=yes --no-install-recommends \
+	${KBUILD_DEPENDS}
+
+cd $1
+make olddefconfig
+
+rm -f .version
+make -j $(($(nproc) * 2)) deb-pkg
+
+rm -rf ${REPACK_DIR}
+mkdir -p ${REPACK_DIR}
+mkdir -p ${REPACK_LINUX_IMAGE_DIR}
+mkdir -p ${REPACK_LINUX_HEADERS_DIR}
+
+cd ..
+tar xzf linux-${PV}_${PV}-1.debian.tar.gz -C ${REPACK_DIR}
+dpkg-deb -R linux-image-${PV}_${PV}-1_*.deb ${REPACK_LINUX_IMAGE_DIR}
+dpkg-deb -R linux-headers-${PV}_${PV}-1_*.deb ${REPACK_LINUX_HEADERS_DIR}
+
+dpkg-gencontrol -crepack/debian/control \
+	-lrepack/debian/changelog \
+	-frepack/debian/files \
+	-plinux-image-${PV} \
+	-P${REPACK_LINUX_IMAGE_DIR} \
+	-DPackage="linux-image-${KERNEL_NAME}" \
+	-DSection=kernel \
+	-DPriority=required \
+	-DDepends="${KERNEL_DEBIAN_DEPENDS}"
+
+# Add Debian-like link installation to postinst
+touch ${REPACK_LINUX_IMAGE_DIR}/lib/modules/${PV}/.fresh-install
+sed -i ${REPACK_LINUX_IMAGE_DIR}/DEBIAN/postinst \
+    -e "/^set -e$/a\\
+\\
+if [ -f /lib/modules/${PV}/.fresh-install ]; then\\
+	change=install\\
+else\\
+	change=upgrade\\
+fi\\
+linux-update-symlinks \$change ${PV} /boot/vmlinuz-${PV}\\
+rm -f /lib/modules/${PV}/.fresh-install"
+
+# Add Debian-like link removal to postrm
+sed -i ${REPACK_LINUX_IMAGE_DIR}/DEBIAN/postrm \
+    -e "/^set -e$/a\\
+\\
+rm -f /lib/modules/${PV}/.fresh-install\\
+\\
+if [ \"\$1\" != upgrade ] && command -v linux-update-symlinks >/dev/null; then\\
+	linux-update-symlinks remove ${PV}  /boot/vmlinuz-${PV}\\
+fi"
+
+dpkg-gencontrol -crepack/debian/control \
+	-lrepack/debian/changelog \
+	-frepack/debian/files \
+	-plinux-headers-${PV} \
+	-P${REPACK_LINUX_HEADERS_DIR} \
+	-Vkernel:debarch="${KERNEL_NAME}" \
+	-DPackage="linux-headers-${KERNEL_NAME}" \
+	-DSection=kernel \
+	-DDepends="${KERNEL_HEADERS_DEBIAN_DEPENDS}"
+
+dpkg-deb -b ${REPACK_LINUX_IMAGE_DIR} \
+	linux-image-${KERNEL_NAME}_${PV}-1_${KERNEL_NAME}.deb
+rm -f linux-image-${PV}_${PV}-1_*.deb
+dpkg-deb -b ${REPACK_LINUX_HEADERS_DIR} \
+	linux-headers-${KERNEL_NAME}_${PV}-1_${KERNEL_NAME}.deb
+rm -f linux-headers-${PV}_${PV}-1_*.deb
