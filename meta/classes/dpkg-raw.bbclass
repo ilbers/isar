ilbers/isar

commit b04530d19c790a57ded3da5de1cb6606e19b314e

dpkg-raw: Clean DEBIAN dir prior to filling it
When we run unprivileged, writing files in that directory will not work
after the first run because everything is root-owned at the end.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/dpkg-raw.bbclass
@@ -16,10 +16,9 @@ do_install() {
 do_install[stamp-extra-info] = "${DISTRO}-${DISTRO_ARCH}"
 addtask install after do_unpack before do_deb_package_prepare
 
-# so we can put hooks etc. in there already
-do_install[dirs] = "${D}/DEBIAN"
-
 do_deb_package_prepare() {
+	sudo rm -rf ${D}/DEBIAN
+	mkdir ${D}/DEBIAN
 	cat<<-__EOF__ > ${D}/DEBIAN/control
 		Package: ${PN}
 		Architecture: ${DISTRO_ARCH}

commit 3394a039b8ed16f95b8de7b1ed9d097bb2c0ba21

dpkg-raw: Fix creation of DEBIAN folder
If do_install did not do this for us, we failed.

Fixes: b04530d19c79 (dpkg-raw: Clean DEBIAN dir prior to filling it)
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/dpkg-raw.bbclass
@@ -18,7 +18,7 @@ addtask install after do_unpack before do_deb_package_prepare
 
 do_deb_package_prepare() {
 	sudo rm -rf ${D}/DEBIAN
-	mkdir ${D}/DEBIAN
+	mkdir -p ${D}/DEBIAN
 	cat<<-__EOF__ > ${D}/DEBIAN/control
 		Package: ${PN}
 		Architecture: ${DISTRO_ARCH}