e9a5d8adf6b55e536aff066c66b68c5d08b4b8fb
Cedric Hombourger <Cedric_Hombourger@mentor.com>
2018-08-26 08:21:28 +0200

    dpkg+linux: use Isar's lock via do_prepare[lockfiles] instead of flock
    reprepro and apt operations may not run in parallel since this could result
    in the apt database being updated (by reprepro) while it is being read (by
    apt-get update). reprepro calls were already serialized with Isar's lock (via
    do_deploy_deb[lockfiles]). Use the same lock for do_prepare in the dpkg and
    linux-custom packaging code. This allows us to drop the /dpkg.lock flock and
    use bitbake's locking scheme instead.
    
    Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

--- meta/classes/dpkg.bbclass
+++ meta/classes/dpkg.bbclass
@@ -9,6 +9,9 @@ dpkg_prepare() {
     sudo -E chroot ${BUILDCHROOT_DIR} /isar/deps.sh ${PP}/${PPS} ${DISTRO_ARCH}
 }
 
+# apt and reprepro may not run in parallel, acquire the Isar lock
+do_prepare[lockfiles] += "${DEPLOY_DIR_APT}/isar.lock"
+
 # Build package from sources using build script
 dpkg_runbuild() {
     E="${@ bb.utils.export_proxies(d)}"
--- meta/recipes-devtools/buildchroot/files/deps.sh
+++ meta/recipes-devtools/buildchroot/files/deps.sh
@@ -12,21 +12,16 @@ source /isar/common.sh
 #   2) we add -y to go non-interactive
 install_cmd="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"
 
-(
-    # Lock-protected because apt and dpkg do not wait in case of contention
-    flock 42 || exit 1
+# Make sure that we have latest isar-apt content.
+# Options meaning:
+#   Dir::Etc::sourcelist - specifies which source to be used
+#   Dir::Etc::sourceparts - disables looking for the other sources
+#   APT::Get::List-Cleanup - do not erase obsolete packages list for
+#                            upstream in '/var/lib/apt/lists'
+apt-get update \
+    -o Dir::Etc::sourcelist="sources.list.d/isar-apt.list" \
+    -o Dir::Etc::sourceparts="-" \
+    -o APT::Get::List-Cleanup="0"
 
-    # Make sure that we have latest isar-apt content.
-    # Options meaning:
-    #   Dir::Etc::sourcelist - specifies which source to be used
-    #   Dir::Etc::sourceparts - disables looking for the other sources
-    #   APT::Get::List-Cleanup - do not erase obsolete packages list for
-    #                            upstream in '/var/lib/apt/lists'
-    apt-get update \
-        -o Dir::Etc::sourcelist="sources.list.d/isar-apt.list" \
-        -o Dir::Etc::sourceparts="-" \
-        -o APT::Get::List-Cleanup="0"
-
-    # Install all build deps
-    mk-build-deps $set_arch -t "${install_cmd}" -i -r debian/control
-) 42>/dpkg.lock
+# Install all build deps
+mk-build-deps $set_arch -t "${install_cmd}" -i -r debian/control
