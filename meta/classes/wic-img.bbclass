ilbers/isar

commit f13b2bf71dd841eaebbbcd04f14a7fbcb2649572

wic-image: Fix up file ownership in isar repo after wic run
This prevents needlessly leaving the __pycache__ directories root-owned
behind after running wic as superuser - will only cause troubles to the
user when trying to clean up the isar repo.

We update the ownership only for selected subdirs of the repo to avoid
touching local files.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/wic-img.bbclass
@@ -71,6 +71,7 @@ do_wic_image() {
     export MTOOLS_SKIP_CHECK=1
 
     sudo -E chroot ${BUILDCHROOT_DIR} ${ISARROOT}/scripts/wic create ${WKS_FILE} --vars "${STAGING_DIR}/${MACHINE}/imgdata/" -o /tmp/ -e ${IMAGE_BASENAME} ${WIC_CREATE_EXTRA_ARGS}
+    sudo chown -R $(stat -c "%U" ${ISARROOT}) ${ISARROOT}/meta ${ISARROOT}/meta-isar ${ISARROOT}/scripts
     cp -f `ls -t -1 ${BUILDCHROOT_DIR}/tmp/${WKS_FILE}*.direct | head -1` ${WIC_IMAGE_FILE}
 }
 

commit 0d1c78ac6080d6bfc2c7eea2787b55f729d47727

wic-img: Allow chown to fail
f13b2bf71dd841 introduced chowning of possible python cache files. This
operation can fail if the file system where Isar resides is read-only,
in which case there will also be no python cache files.

Simply allow this step to fail, even other failures would not be
problematic because it is a convenience thing to execute the chown.

Signed-off-by: Henning Schild <henning.schild@siemens.com>

meta/classes/wic-img.bbclass
@@ -76,7 +76,7 @@ do_wic_image() {
     export MTOOLS_SKIP_CHECK=1
 
     sudo -E chroot ${BUILDCHROOT_DIR} ${ISARROOT}/scripts/wic create ${WKS_FILE} --vars "${STAGING_DIR}/${MACHINE}/imgdata/" -o /tmp/ -e ${IMAGE_BASENAME} ${WIC_CREATE_EXTRA_ARGS}
-    sudo chown -R $(stat -c "%U" ${ISARROOT}) ${ISARROOT}/meta ${ISARROOT}/meta-isar ${ISARROOT}/scripts
+    sudo chown -R $(stat -c "%U" ${ISARROOT}) ${ISARROOT}/meta ${ISARROOT}/meta-isar ${ISARROOT}/scripts || true
     cp -f `ls -t -1 ${BUILDCHROOT_DIR}/tmp/${WKS_FILE}*.direct | head -1` ${WIC_IMAGE_FILE}
 }
 