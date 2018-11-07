ilbers/isar

commit be291cd991bd64051f5e34af24b04feddaaea48c

buildchroot: build debian packages as "builder" not "root"
We used to build packages as "root" and now do that as a regular user.
Not building as "root" allows us to find mistakes in debian/rules where
privileged operations are used while they should not (a sudo was
found in a rules-file). Further some build steps might actually expect
to not run as root (seen in openssl test suite).

Not building as root should increase overall quality and brings us
closer to how debian packages are build by others.

Signed-off-by: Henning Schild <henning.schild@siemens.com>

meta/recipes-devtools/buildchroot/files/build.sh
@@ -14,5 +14,7 @@ for i in configure aclocal.m4 Makefile.am Makefile.in; do
     fi
 done
 
-# Build the package
-dpkg-buildpackage -a$target_arch -d --source-option=-I
+# Build the package as user "builder"
+chown -R builder:builder $1 # the sources
+chown builder:builder $1/.. # the output
+su - builder -c "cd $1; dpkg-buildpackage -a$target_arch -d --source-option=-I"

meta/recipes-devtools/buildchroot/buildchroot-target.bb
@@ -18,6 +18,7 @@ BUILDCHROOT_PREINSTALL ?= "gcc \
                            apt \
                            automake \
                            devscripts \
-                           equivs"
+                           equivs \
+                           adduser"
 
 do_build[depends] = "isar-apt:do_cache_config isar-bootstrap-target:do_bootstrap"

meta/recipes-devtools/buildchroot/files/configscript.sh
@@ -9,3 +9,7 @@ debconf-set-selections <<END
 locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8
 locales locales/default_environment_locale select en_US.UTF-8
 END
+
+addgroup --quiet --system builder
+useradd --system --gid builder --no-create-home --home /home/builder --no-user-group --comment "Isar buildchroot build user" builder
+chown -R builder:builder /home/builder

commit 7f0d2c4288227e0e20773b8358b0631aa91c6f7d

meta: builchroot: Preserve environment when calling dpkg-buildpackage
This is required in order to allow recipes to pass variables down to the
rules file. E.g., u-boot-custom.inc makes use of this.

Fixes: be291cd991bd ("buildchroot: build debian packages as "builder" not "root"")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/recipes-devtools/buildchroot/files/build.sh
@@ -17,4 +17,4 @@ done
 # Build the package as user "builder"
 chown -R builder:builder $1 # the sources
 chown builder:builder $1/.. # the output
-su - builder -c "cd $1; dpkg-buildpackage -a$target_arch -d --source-option=-I"
+su builder -c "cd $1; dpkg-buildpackage -a$target_arch -d --source-option=-I"