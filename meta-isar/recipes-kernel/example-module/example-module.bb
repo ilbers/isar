ilbers/isar

commit 301f5237805260b19135c93b13fd2fa18ca1fef2

meta-isar: example-module: Only disable cross-compilation for Debian kernels
It's perfectly fine to cross-build this module with self-built kernels.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta-isar/recipes-kernel/example-module/example-module.bb
@@ -7,15 +7,18 @@
 
 include recipes-kernel/linux-module/module.inc
 
-SRC_URI += "file://src"
-
-S = "${WORKDIR}/src"
-
-AUTOLOAD = "${PN}"
-
 # Cross-compilation is not supported for the default Debian kernels.
 # For example, package with kernel headers for ARM:
 #   linux-headers-armmp
 # has hard dependencies from linux-compiler-gcc-4.8-arm, what
 # conflicts with the host binaries.
-ISAR_CROSS_COMPILE = "0"
+python() {
+    if d.getVar('KERNEL_NAME') in ['armmp', 'arm64', 'rpi-rpfv', 'amd64', '686-pae']:
+        d.setVar('ISAR_CROSS_COMPILE', '0')
+}
+
+SRC_URI += "file://src"
+
+S = "${WORKDIR}/src"
+
+AUTOLOAD = "${PN}"

commit 676ca1ca8012fbc77b2a904c29c1d28d8322b323

meta-isar: example-module: Fix ISAR_CROSS_COMPILE setting
Make sure ISAR_CROSS_COMPILE is updated prior to inheriting buildchroot
indirectly via module.inc. Otherwise, setting it to 0 will come too
late.

Fixes: 301f52378052 ("meta-isar: example-module: Only disable cross-compilation for Debian kernels")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta-isar/recipes-kernel/example-module/example-module.bb
@@ -5,8 +5,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-include recipes-kernel/linux-module/module.inc
-
 # Cross-compilation is not supported for the default Debian kernels.
 # For example, package with kernel headers for ARM:
 #   linux-headers-armmp
@@ -16,6 +14,8 @@ python() {
     if d.getVar('KERNEL_NAME') in ['armmp', 'arm64', 'rpi-rpfv', 'amd64', '686-pae']:
         d.setVar('ISAR_CROSS_COMPILE', '0')
 }
+
+include recipes-kernel/linux-module/module.inc
 
 SRC_URI += "file://src"
 