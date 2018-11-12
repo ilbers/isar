ilbers/isar

commit d702b4cceb64be7eef13a8bc3f306b6c97a053ed

base-apt: Add helper class
base-apt intended to store original upstream debs to re-use them
later offline. This class helps to populate base-apt with the packages
used during build.

Signed-off-by: Alexander Smirnov <asmirnov@ilbers.de>
Reviewed-by: Claudius Heine <ch@denx.de>
Signed-off-by: Maxim Yu. Osipov <mosipov@ilbers.de>

meta/classes/base-apt-helper.bbclass
@@ -1,0 +1,54 @@
+# This software is a part of ISAR.
+# Copyright (C) 2018 ilbers GmbH
+
+DISTRO_NAME ?= "${@ d.getVar('DISTRO', True).split('-')[0]}"
+DISTRO_SUITE ?= "${@ d.getVar('DISTRO', True).split('-')[1]}"
+
+populate_base_apt() {
+    search_dir=$1
+
+    find $search_dir -name '*.deb' | while read package; do
+        # NOTE: due to packages stored by reprepro are not modified, we can
+        # use search by filename to check if package is already in repo. In
+        # addition, m5sums could be compared to ensure, that package is the
+        # same and should not be overwritten. This method is easier and more
+        # robust than querying reprepro by name.
+
+        # Check if this package is taken from Isar-apt, if so - ingore it.
+        base_name=${package##*/}
+        isar_package=$(find ${DEPLOY_DIR_APT}/${DISTRO} -name $base_name)
+        if [ -n "$isar_package" ]; then
+            # Check if MD5 sums are identical. This helps to avoid the case
+            # when packages is overridden from another repo.
+            md1=$(md5sum $package | cut -d ' ' -f 1)
+            md2=$(md5sum $isar_package | cut -d ' ' -f 1)
+            if [ "$md1" = "$md2" ]; then
+                continue
+            fi
+        fi
+
+        # Check if this package is already in base-apt
+        isar_package=$(find ${REPO_BASE_DIR}/${DISTRO_NAME} -name $base_name)
+        if [ -n "$isar_package" ]; then
+            md1=$(md5sum $package | cut -d ' ' -f 1)
+            md2=$(md5sum $isar_package | cut -d ' ' -f 1)
+            if [ "$md1" = "$md2" ]; then
+                continue
+            fi
+
+            # md5sum differs, so remove the package from base-apt
+            name=$($base_name | cut -d '_' -f 1)
+            reprepro -b ${REPO_BASE_DIR}/${DISTRO_NAME} \
+                     --dbdir ${REPO_BASE_DB_DIR}/${DISTRO_NAME} \
+                     -C main -A ${DISTRO_ARCH} \
+                     remove ${DISTRO_SUITE} \
+                     $name
+        fi
+
+        reprepro -b ${REPO_BASE_DIR}/${DISTRO_NAME} \
+                 --dbdir ${REPO_BASE_DB_DIR}/${DISTRO_NAME} \
+                 -C main \
+                 includedeb ${DISTRO_SUITE} \
+                 $package
+    done
+}

meta-isar/conf/layer.conf
@@ -25,3 +25,7 @@ DEPLOY_DIR_APT ?= "${DEPLOY_DIR}/apt"
 
 # Path to the Isar databases used by `reprepro`
 DEPLOY_DIR_DB ?= "${DEPLOY_DIR}/db"
+
+# Base apt repository paths
+REPO_BASE_DIR ?= "${DEPLOY_DIR}/base-apt/apt"
+REPO_BASE_DB_DIR ?= "${DEPLOY_DIR}/base-apt/db"

commit bff4e89a9861e35b35048471d91115622d251a42

base-apt: Fix distro and codename retrieval
So far, the base-apt-helper derived the base distro name and codename
(so far called suite, but it was already the codename) from the DISTRO
variable. That broke downstream layers which defined their own DISTRO.

A better approach is to let the distro config file define those
parameters so that they can reliably propagate, also via derived
distros, to the helper tasks.

Fixes: d702b4cceb64 ("base-apt: Add helper class")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta-isar/conf/distro/debian-buster.conf
@@ -2,6 +2,8 @@
 
 require debian-common.conf
 
+BASE_DISTRO_CODENAME = "buster"
+
 DISTRO_APT_SOURCES += "conf/distro/debian-buster.list"
 DISTRO_CONFIG_SCRIPT ?= "debian-configscript.sh"
 DISTRO_KERNELS ?= "4kc-malta 5kc-malta 686 686-pae amd64 arm64 armmp \

meta-isar/conf/distro/debian-common.conf
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: MIT
 
+BASE_DISTRO = "debian"
+
 WIC_IMAGER_INSTALL = "parted \
                       gdisk \
                       util-linux \

meta-isar/conf/distro/debian-jessie.conf
@@ -5,6 +5,8 @@
 
 require debian-common.conf
 
+BASE_DISTRO_CODENAME = "jessie"
+
 DISTRO_APT_SOURCES += "conf/distro/debian-jessie.list"
 DISTRO_CONFIG_SCRIPT ?= "debian-configscript.sh"
 DISTRO_KERNELS ?= "486 4kc-malta 586 5kc-malta 686-pae amd64 arm64 armmp \

meta-isar/conf/distro/debian-stretch.conf
@@ -3,6 +3,8 @@
 
 require debian-common.conf
 
+BASE_DISTRO_CODENAME = "stretch"
+
 DISTRO_APT_SOURCES += "conf/distro/debian-stretch.list"
 DISTRO_CONFIG_SCRIPT ?= "debian-configscript.sh"
 DISTRO_KERNELS ?= "4kc-malta 586 5kc-malta 686 686-pae amd64 arm64 armmp \

meta-isar/conf/distro/raspbian-jessie.conf
@@ -3,6 +3,9 @@
 # This software is a part of ISAR.
 # Copyright (C) 2015-2016 ilbers GmbH
 
+BASE_DISTRO = "raspbian"
+BASE_DISTRO_CODENAME = "jessie"
+
 DISTRO_APT_SOURCES += "conf/distro/raspbian-jessie.list"
 DISTRO_APT_KEYS += "https://archive.raspbian.org/raspbian.public.key;sha256sum=ca59cd4f2bcbc3a1d41ba6815a02a8dc5c175467a59bd87edeac458f4a5345de"
 DISTRO_CONFIG_SCRIPT?= "raspbian-configscript.sh"

meta/classes/base-apt-helper.bbclass
@@ -1,9 +1,6 @@
 # This software is a part of ISAR.
 # Copyright (C) 2018 ilbers GmbH
 
-DISTRO_NAME ?= "${@ d.getVar('DISTRO', True).split('-')[0]}"
-DISTRO_SUITE ?= "${@ d.getVar('DISTRO', True).split('-')[1]}"
-
 compare_pkg_md5sums() {
    pkg1=$1
    pkg2=$2
@@ -34,23 +31,23 @@ populate_base_apt() {
         fi
 
         # Check if this package is already in base-apt
-        isar_package=$(find ${REPO_BASE_DIR}/${DISTRO_NAME} -name $base_name)
+        isar_package=$(find ${REPO_BASE_DIR}/${BASE_DISTRO} -name $base_name)
         if [ -n "$isar_package" ]; then
             compare_pkg_md5sums "$package" "$isar_package" && continue
 
             # md5sum differs, so remove the package from base-apt
             name=$(echo $base_name | cut -d '_' -f 1)
-            reprepro -b ${REPO_BASE_DIR}/${DISTRO_NAME} \
-                     --dbdir ${REPO_BASE_DB_DIR}/${DISTRO_NAME} \
+            reprepro -b ${REPO_BASE_DIR}/${BASE_DISTRO} \
+                     --dbdir ${REPO_BASE_DB_DIR}/${BASE_DISTRO} \
                      -C main -A ${DISTRO_ARCH} \
-                     remove ${DISTRO_SUITE} \
+                     remove ${BASE_DISTRO_CODENAME} \
                      $name
         fi
 
-        reprepro -b ${REPO_BASE_DIR}/${DISTRO_NAME} \
-                 --dbdir ${REPO_BASE_DB_DIR}/${DISTRO_NAME} \
+        reprepro -b ${REPO_BASE_DIR}/${BASE_DISTRO} \
+                 --dbdir ${REPO_BASE_DB_DIR}/${BASE_DISTRO} \
                  -C main \
-                 includedeb ${DISTRO_SUITE} \
+                 includedeb ${BASE_DISTRO_CODENAME} \
                  $package
     done
 }

meta/recipes-core/isar-bootstrap/files/base-apt-sources
@@ -1,1 +1,1 @@
-deb [trusted=yes] file:///base-apt/debian {DISTRO} main
+deb [trusted=yes] file:///base-apt/{BASE_DISTRO} {BASE_DISTRO_CODENAME} main

meta/recipes-core/isar-bootstrap/isar-bootstrap.inc
@@ -211,7 +211,9 @@ isar_bootstrap() {
                              "${ROOTFSDIR}/etc/apt/preferences.d/bootstrap"
             mkdir -p "${ROOTFSDIR}/etc/apt/sources.list.d"
             if [ "${ISAR_USE_CACHED_BASE_REPO}" = "1" ]; then
-                sed -i -e "s#{DISTRO}#"${DISTRO_SUITE}"#g" ${BASEAPTSRCS}
+                sed -e "s#{BASE_DISTRO}#"${BASE_DISTRO}"#g" \
+                    -e "s#{BASE_DISTRO_CODENAME}#"${BASE_DISTRO_CODENAME}"#g" \
+	            -i ${BASEAPTSRCS}
                 mkdir -p ${ROOTFSDIR}/base-apt
                 sudo mount --bind ${REPO_BASE_DIR} ${ROOTFSDIR}/base-apt
                 install -v -m644 "${BASEAPTSRCS}" \

meta/recipes-devtools/base-apt/base-apt.bb
@@ -5,7 +5,7 @@ SRC_URI = "file://distributions.in"
 
 inherit base-apt-helper
 
-CACHE_CONF_DIR = "${REPO_BASE_DIR}/${DISTRO_NAME}/conf"
+CACHE_CONF_DIR = "${REPO_BASE_DIR}/${BASE_DISTRO}/conf"
 do_cache_config[dirs] = "${CACHE_CONF_DIR}"
 do_cache_config[stamp-extra-info] = "${DISTRO}"
 do_cache_config[lockfiles] = "${REPO_BASE_DIR}/isar.lock"
@@ -14,17 +14,17 @@ do_cache_config[lockfiles] = "${REPO_BASE_DIR}/isar.lock"
 # generated, this task should do nothing.
 do_cache_config() {
     if [ ! -e "${CACHE_CONF_DIR}/distributions" ]; then
-        sed -e "s#{DISTRO_NAME}#"${DISTRO_SUITE}"#g" \
+        sed -e "s#{CODENAME}#"${BASE_DISTRO_CODENAME}"#g" \
             ${WORKDIR}/distributions.in > ${CACHE_CONF_DIR}/distributions
     fi
 
-    path_cache="${REPO_BASE_DIR}/${DISTRO_NAME}"
-    path_databases="${REPO_BASE_DB_DIR}/${DISTRO_NAME}"
+    path_cache="${REPO_BASE_DIR}/${BASE_DISTRO}"
+    path_databases="${REPO_BASE_DB_DIR}/${BASE_DISTRO}"
 
     if [ ! -d "${path_databases}" ]; then
         reprepro -b ${path_cache} \
                  --dbdir ${path_databases} \
-                 export ${DISTRO_SUITE}
+                 export ${BASE_DISTRO_CODENAME}
     fi
 }
 

meta/recipes-devtools/base-apt/files/distributions.in
@@ -1,3 +1,3 @@
-Codename: {DISTRO_NAME}
+Codename: {CODENAME}
 Architectures: i386 armhf arm64 amd64 source
 Components: main