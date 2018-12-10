ilbers/isar

commit 05d0fc43e9e9fec86ff2fa5d987480d661abaf4f

isar-events: Improve umount handler
Using lazy umount avoids having to loop over the mount points.
Furthermore, also triggering on runQueueExitWait allows to perform the
umount earlier. Finally, filter for the events we actually want.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/isar-events.bbclass
@@ -6,34 +6,22 @@
 
 addhandler isar_handler
 
-python isar_handler () {
+python isar_handler() {
     import subprocess
-    import time
+    import bb.runqueue
 
-    def umount_all(basepath):
-        # '/proc/mounts' contains all the active mounts, so knowing basepath
-        # we can get the list of mounts for the specific multiconfig and
-        # clean them.
-        result = True
+    tmpdir = d.getVar('TMPDIR', True)
+    if not tmpdir:
+        return
+
+    basepath = tmpdir + '/work/'
+
+    with open(os.devnull, 'w') as devnull:
         with open('/proc/mounts', 'rU') as f:
             for line in f:
                 if basepath in line:
-                    if subprocess.call('sudo umount ' + line.split()[1],
-                                       stdout=devnull, stderr=devnull,
-                                       shell=True) != 0:
-                        result = False
-        return result
-
-    devnull = open(os.devnull, 'w')
-
-    if isinstance(e, bb.event.BuildCompleted):
-        tmpdir = d.getVar('TMPDIR', True)
-
-        if tmpdir:
-            basepath = tmpdir + '/work/'
-
-            while not umount_all(basepath):
-                time.sleep(1)
-
-    devnull.close()
+                    subprocess.call('sudo umount -l ' + line.split()[1],
+                                    stdout=devnull, stderr=devnull, shell=True)
 }
+
+isar_handler[eventmask] = "bb.runqueue.runQueueExitWait bb.event.BuildCompleted"

commit 3020d2a2cfca546bd47f52dc403d660655a200d8

isar-events: Fix processing of /proc/mounts
We have to read /proc/mounts completely before starting to umount. That
virtual file will change after each umount, and that could make us miss
some mount points.

Fixes: 05d0fc43e9e9 ("isar-events: Improve umount handler")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/isar-events.bbclass
@@ -18,10 +18,11 @@ python isar_handler() {
 
     with open(os.devnull, 'w') as devnull:
         with open('/proc/mounts', 'rU') as f:
-            for line in f:
-                if basepath in line:
-                    subprocess.call('sudo umount -l ' + line.split()[1],
-                                    stdout=devnull, stderr=devnull, shell=True)
+            lines = f.readlines()
+        for line in lines:
+            if basepath in line:
+                subprocess.call('sudo umount -l ' + line.split()[1],
+                                stdout=devnull, stderr=devnull, shell=True)
 }
 
 isar_handler[eventmask] = "bb.runqueue.runQueueExitWait bb.event.BuildCompleted"