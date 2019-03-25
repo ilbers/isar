ilbers/isar

commit 43b4c21aa1853b0c8fdffe1de7d87ecf00d8612d

meta: added do_transform_template task as templating system and switch
The new introduced variables are TEMPLATE_FILES and TEMPLATE_VARS.
TEMPLATE_FILES is a list of files that should be processed in the
`do_transform_template` task and TEMPLATE_VARS is a list of bitbake
variable names that should be substituted in the template file.

Signed-off-by: Claudius Heine <ch@denx.de>

meta/classes/template.bbclass
@@ -1,0 +1,62 @@
+# This software is a part of ISAR.
+# Copyright (C) Siemens AG, 2019
+#
+# SPDX-License-Identifier: MIT
+
+TEMPLATE_FILES ?= ""
+TEMPLATE_VARS ?= "PN PV DESCRIPTION HOMEPAGE MAINTAINER KERNEL_NAME MACHINE \
+                   DISTRO_ARCH"
+
+do_transform_template[vardeps] = "TEMPLATE_FILES ${TEMPLATE_VARS}"
+do_transform_template[stamp-extra-info] = "${DISTRO}-${DISTRO_ARCH}"
+python do_transform_template() {
+    import subprocess, contextlib
+
+    workdir = os.path.normpath(d.getVar('WORKDIR', True))
+
+    template_vars = (d.getVar('TEMPLATE_VARS', True) or "").split()
+    if len(template_vars) == 0:
+        return
+
+    template_files = (d.getVar('TEMPLATE_FILES', True) or "").split()
+    if len(template_files) == 0:
+        return
+
+    cmd = "envsubst"
+    args = " ".join("\${{{}}}".format(i) for i in template_vars)
+
+    # Copy current process environment and add template variables
+    # from bitbake data store:
+    env = os.environ.copy()
+    for varname in template_vars:
+        value = d.getVar(varname, True)
+        if value:
+            env.update({varname: value})
+
+    for template_file in template_files:
+        # Normpath and workdir checks should prevent accidential or
+        # uninformed changes to files outside of the tmp and workdirectoy
+        template_file = os.path.normpath(template_file)
+
+        # Convert relative paths to absolut paths based on the workdir:
+        if not os.path.isabs(template_file):
+            template_file = os.path.normpath(os.path.join(workdir, template_file))
+
+        if not template_file.startswith(workdir):
+            bb.fatal("Template file ({}) is not within workdir ({})"
+                     .format(template_file, workdir))
+
+        output_file = (os.path.splitext(template_file)[0]
+                       if template_file.endswith(".tmpl")
+                       else (template_file + ".out"))
+        bb.note("{} {} [in: {} out: {}]".format(cmd, args,
+                                                template_file, output_file))
+        with contextlib.ExitStack() as stack:
+            input = stack.enter_context(open(template_file, 'rb'))
+            output = stack.enter_context(open(output_file, 'wb'))
+            process = stack.enter_context(subprocess.Popen([cmd, args], stdin=input,
+                                          stdout=output, env=env))
+            if process.wait() != 0:
+                bb.fatal("processing of template failed")
+}
+addtask do_transform_template after do_unpack

meta/recipes-bsp/u-boot/u-boot-custom.inc
@@ -15,16 +15,14 @@ inherit dpkg
 
 SRC_URI += "file://debian/"
 
-U_BOOT_BUILD_DEPENDS ?= "bc, bison, flex, device-tree-compiler"
+BUILD_DEPENDS ?= "bc, bison, flex, device-tree-compiler"
+
+TEMPLATE_FILES = "debian/changelog.tmpl \
+                  debian/control.tmpl"
+TEMPLATE_VARS += "BUILD_DEPENDS"
 
 do_prepare_build() {
     cp -r ${WORKDIR}/debian ${S}/
-    sed -i -e 's/@PN@/${PN}/g' -e 's/@PV@/${PV}/g' \
-        -e 's/@BUILD_DEPENDS@/${U_BOOT_BUILD_DEPENDS}/g' \
-        -e 's/@MACHINE@/${MACHINE}/g' \
-        -e 's/@DISTRO_ARCH@/${DISTRO_ARCH}/g' \
-        -e 's/@DESCRIPTION@/${DESCRIPTION}/g' \
-        ${S}/debian/changelog ${S}/debian/control
 
     echo "${U_BOOT_BIN} /usr/lib/u-boot/${MACHINE}" > \
         ${S}/debian/u-boot-${MACHINE}.install

meta/classes/base.bbclass
@@ -25,6 +25,7 @@ die() {
 }
 
 inherit logging
+inherit template
 
 # Derived from bitbake: bitbake/classes/base.bbclass
 addtask showdata

meta/classes/dpkg-base.bbclass
@@ -60,7 +60,7 @@ do_prepare_build() {
     true
 }
 
-addtask prepare_build after do_patch before do_build
+addtask prepare_build after do_patch do_transform_template before do_build
 do_prepare_build[stamp-extra-info] = "${DISTRO}-${DISTRO_ARCH}"
 # If Isar recipes depend on each other, they typically need the package
 # deployed to isar-apt

meta/recipes-bsp/u-boot/files/debian/control
@@ -1,19 +1,0 @@
-Source: @PN@
-Section: admin
-Priority: optional
-Standards-Version: 3.9.6
-Build-Depends: @BUILD_DEPENDS@
-Maintainer: ISAR project <isar-users@googlegroups.com>
-
-Package: u-boot-@MACHINE@
-Architecture: @DISTRO_ARCH@
-Description: @DESCRIPTION@, bootloader binaries
-
-Package: u-boot-@MACHINE@-dev
-Architecture: @DISTRO_ARCH@
-Description: @DESCRIPTION@, bootloader libraries
-
-Package: u-boot-tools
-Architecture: linux-any
-Depends: ${shlibs:Depends}, ${misc:Depends}
-Description: @DESCRIPTION@, companion tools

meta/recipes-bsp/u-boot/files/debian/control.tmpl
@@ -1,0 +1,19 @@
+Source: ${PN}
+Section: admin
+Priority: optional
+Standards-Version: 3.9.6
+Build-Depends: ${BUILD_DEPENDS}
+Maintainer: ISAR project <isar-users@googlegroups.com>
+
+Package: u-boot-${MACHINE}
+Architecture: ${DISTRO_ARCH}
+Description: ${DESCRIPTION}, bootloader binaries
+
+Package: u-boot-${MACHINE}-dev
+Architecture: ${DISTRO_ARCH}
+Description: ${DESCRIPTION}, bootloader libraries
+
+Package: u-boot-tools
+Architecture: linux-any
+Depends: ${shlibs:Depends}, ${misc:Depends}
+Description: ${DESCRIPTION}, companion tools

meta/recipes-kernel/linux-module/files/debian/control
@@ -1,11 +1,0 @@
-Source: @PN@
-Section: kernel
-Priority: optional
-Standards-Version: 3.9.6
-Build-Depends: linux-headers-@KERNEL_NAME@
-Maintainer: ISAR project <isar-users@googlegroups.com>
-
-Package: @PN@
-Architecture: any
-Depends: linux-image-@KERNEL_NAME@, kmod
-Description: @DESCRIPTION@

meta/recipes-kernel/linux-module/files/debian/control.tmpl
@@ -1,0 +1,11 @@
+Source: ${PN}
+Section: kernel
+Priority: optional
+Standards-Version: 3.9.6
+Build-Depends: linux-headers-${KERNEL_NAME}
+Maintainer: ISAR project <isar-users@googlegroups.com>
+
+Package: ${PN}
+Architecture: any
+Depends: linux-image-${KERNEL_NAME}, kmod
+Description: ${DESCRIPTION}

commit d46a7c390718c2af6aefbcf184ec41aa1c3ce08f

template: Remove MACHINE from default TEMPLATE_VARS
This breaks packages, even those not using templating, when they are
pulled via multiconfig by different machines of the same arch. As the
MACHINE variable differs, bitbake first builds do_transform_template
multiple times, and then everything that follows after, including
standard packages like isar-cfg-* or expand-on-first-boot. Those are
shared, use only a single workdir, and will quickly break of built
concurrently.

Fixes: 43b4c21aa185 ("meta: added do_transform_template task as templating system and switch")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/template.bbclass
@@ -4,8 +4,7 @@
 # SPDX-License-Identifier: MIT
 
 TEMPLATE_FILES ?= ""
-TEMPLATE_VARS ?= "PN PV DESCRIPTION HOMEPAGE MAINTAINER KERNEL_NAME MACHINE \
-                   DISTRO_ARCH"
+TEMPLATE_VARS ?= "PN PV DESCRIPTION HOMEPAGE MAINTAINER KERNEL_NAME DISTRO_ARCH"
 
 do_transform_template[vardeps] = "TEMPLATE_FILES ${TEMPLATE_VARS}"
 do_transform_template[stamp-extra-info] = "${DISTRO}-${DISTRO_ARCH}"

meta/recipes-bsp/u-boot/u-boot-custom.inc
@@ -19,7 +19,7 @@ BUILD_DEPENDS ?= "bc, bison, flex, device-tree-compiler, git"
 
 TEMPLATE_FILES = "debian/changelog.tmpl \
                   debian/control.tmpl"
-TEMPLATE_VARS += "BUILD_DEPENDS"
+TEMPLATE_VARS += "MACHINE BUILD_DEPENDS"
 
 do_prepare_build() {
     cp -r ${WORKDIR}/debian ${S}/