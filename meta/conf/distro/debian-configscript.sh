596732aa99c361b756655434bc90e0108e1caa33
Maxim Yu. Osipov <mosipov@ilbers.de>
2019-04-01 16:57:20 +0200

    debian-configscript.sh: Remove creation of console device
    Creation of console device was introduced by commit e7e5915
    with the  comment "on some hosts, ISAR running in QEMU fails with error:
    /init: 326: /init: cannot open /root/dev/console: No such file"
    
    The mentioned problem is not reproduced anymore on supported hosts,
    moreover, it was reported that such creation of /dev/console
    caused races.
    
    Reported-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Maxim Yu. Osipov <mosipov@ilbers.de>

--- meta/conf/distro/debian-configscript.sh
+++ meta/conf/distro/debian-configscript.sh
@@ -8,11 +8,6 @@ set -e
 readonly MACHINE_SERIAL="$1"
 readonly BAUDRATE_TTY="$2"
 
-# Create console device
-if [ ! -e /dev/console ]; then
-     mknod /dev/console c 5 1
-fi
-
 # Enable tty conditionally, systemd does not have the file but its own magic
 if [ -f /etc/inittab ]; then
     echo "T0:23:respawn:/sbin/getty -L $MACHINE_SERIAL $BAUDRATE_TTY vt100" \
