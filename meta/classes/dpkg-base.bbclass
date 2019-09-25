ilbers/isar

commit 568072765b08fafd509e18974c0714649d0edcf9

dpkg: acquire a read (shared) locks while building packages
As debhelper scripts involved during the actual build process
(dpkg-buildpackage) may access the dpkg database, we need to
make sure it does not get modified in the background by other
build tasks for other packages (such as install_build_deps).
An example of a reader is dh_shlibdeps/dpkg-shlibdeps: it
needs to find packages providing the shared libraries the
package depends on to substitute shlibs variables found in its
control file with package dependencies. Acquire the isar-apt
lock as a shared lock to block writers.

Special care may be needed in other classes/users of the
dpkg_runbuild API. Due to some limitations with bitbake (to be
discussed with the maintainers), we could not grab the lock
from dpkg-base. A statement such as "flock <my-lock.lock>
dpkg_run_build" would fail as flock can't exec() dpkg_run_build
(since it is a shell function, not an executable). The popular
shell construct "( flock -n 9; dpkg_run_build ) 9> <my-lock.lock>"
cannot be parsed by bitbake. And finally the lockfiles task
attribute (implemented in bitbake) only handles exclusive locks.

This commit is only addressing dpkg.bbclass. dpkg-raw.bbclass
is left intact as the generated control file does require
dh_shlibdeps. linux-custom.inc was also left out as the Debian
packaging scripts only use low-level packaging tools and do not
use debhelper scripts during packaging (at this time). In
agreement with the Isar project maintainers, we will not grab
the Isar lock from linux-custom.inc to let kernel builds run
in parallel with do_install_deps() and do_deploy_deb() that
may be executed by other bitbake threads.

Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

meta/classes/dpkg.bbclass
@@ -19,5 +19,6 @@ do_install_builddeps[stamp-extra-info] = "${DISTRO}-${DISTRO_ARCH}"
 # Build package from sources using build script
 dpkg_runbuild() {
     E="${@ bb.utils.export_proxies(d)}"
+    flock -s "${REPO_ISAR_DIR}/isar.lock" \
     sudo -E chroot --userspec=$( id -u ):$( id -g ) ${BUILDCHROOT_DIR} /isar/build.sh ${PP}/${PPS} ${DISTRO_ARCH}
 }

commit a171f5e9ef63a84ce0fa05953eb7543759a3c63a

dpkg: Fix shared-locking of build task
Bitbake file locks are not just exclusive flock calls. For whatever
reason, bitbake prefers to delete the lock file before unlocking them.
That obviously creates a nasty race with other locker requests: Those
already waiting will get access to the removed lock, those coming later
will create a new one. To resolve that race, bitbakes lockfile() tests
if the inode of the acquired lock file is identical to the one that is
currently visible via the lock name. If not, it retries the lock. What a
pointless mess...

Our open-coded "flock -s" does not do that, thus will often acquire only
a stale lock. Fix that by using bitbake's services for locking. That
requires to move the lock acquisition at the top level of the task, ie.
do_build. Thus, also dpkg-raw and kernel builds are now under the shared
(read) lock. The better fix for this issue would actually be private
buildchroots for each build, but that will come later.

CC: Cedric Hombourger <Cedric_Hombourger@mentor.com>
Fixes: 568072765b08 ("dpkg: acquire a read (shared) locks while building packages")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/dpkg-base.bbclass
@@ -86,10 +86,13 @@ dpkg_runbuild() {
     die "This should never be called, overwrite it in your derived class"
 }
 
-do_build() {
-    dpkg_do_mounts
-    dpkg_runbuild
-    dpkg_undo_mounts
+python do_build() {
+    lock = bb.utils.lockfile(d.getVar("REPO_ISAR_DIR") + "/isar.lock",
+                             shared=True)
+    bb.build.exec_func("dpkg_do_mounts", d)
+    bb.build.exec_func("dpkg_runbuild", d)
+    bb.build.exec_func("dpkg_undo_mounts", d)
+    bb.utils.unlockfile(lock)
 }
 
 CLEANFUNCS += "repo_clean"

meta/classes/dpkg.bbclass
@@ -18,7 +18,6 @@ do_install_builddeps[lockfiles] += "${REPO_ISAR_DIR}/isar.lock"
 # Build package from sources using build script
 dpkg_runbuild() {
     E="${@ bb.utils.export_proxies(d)}"
-    flock -s "${REPO_ISAR_DIR}/isar.lock" \
-        sudo -E chroot --userspec=$( id -u ):$( id -g ) ${BUILDCHROOT_DIR} \
-             /isar/build.sh ${PP}/${PPS} ${DISTRO_ARCH}
+    sudo -E chroot --userspec=$( id -u ):$( id -g ) ${BUILDCHROOT_DIR} \
+         /isar/build.sh ${PP}/${PPS} ${DISTRO_ARCH}
 }