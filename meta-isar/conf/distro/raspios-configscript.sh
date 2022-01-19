fef55de879ddda56b8987c104792c447bb5244bb
Uladzimir Bely <ubely@ilbers.de>
2022-01-19 07:59:00 +0100

    raspios: resize rootfs on first boot
    Initially, after rpi-sdimg is created, a quite small rootfs partition
    is created. After boot, there is almost no free space.
    
    This patch uses scripts (idea is taken from official raspbian distro)
    that make rootfs partition expanded at first boot.
    
    Signed-off-by: Uladzimir Bely <ubely@ilbers.de>

--- meta-isar/conf/distro/raspios-configscript.sh
+++ meta-isar/conf/distro/raspios-configscript.sh
@@ -19,7 +19,7 @@ EOF
 cat > /boot/cmdline.txt << EOF
 console=${MACHINE_SERIAL},${BAUDRATE_TTY} console=tty1 \
 root=/dev/mmcblk0p2 rootfstype=ext4 fsck.repair=yes \
-rootwait quiet
+rootwait quiet init=/usr/lib/raspi-config/init_resize.sh
 EOF
 
 cat > /etc/fstab << EOF
@@ -27,3 +27,24 @@ proc            /proc           proc    defaults          0       0
 /dev/mmcblk0p1  /boot           vfat    defaults          0       2
 /dev/mmcblk0p2  /               ext4    defaults,noatime  0       1
 EOF
+
+cat > /etc/init.d/resize2fs_once << EOF
+#!/bin/sh
+. /lib/lsb/init-functions
+case "\$1" in
+  start)
+    log_daemon_msg "Starting resize2fs_once"
+    ROOT_DEV=\$(findmnt / -o source -n) &&
+    resize2fs \$ROOT_DEV &&
+    update-rc.d resize2fs_once remove &&
+    rm /etc/init.d/resize2fs_once &&
+    log_end_msg \$?
+    ;;
+  *)
+    echo "Usage: \$0 start" >&2
+    exit 3
+    ;;
+esac
+EOF
+chmod +x /etc/init.d/resize2fs_once
+ln -s ../init.d/resize2fs_once /etc/rc3.d/S01resize2fs_once
