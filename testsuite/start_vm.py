3a6cc70f8dcfa6a8bdac581ddf211865c3f9647f
Felix Moessbauer <felix.moessbauer@siemens.com>
2022-03-22 17:45:24 +0100

    Add pcbios option to start_vm
    This patch adds the --pcbios option to the start_vm script to
    enforce that any -bios QEMU options are removed.
    By that, QEMU is forced to use a pc bios (like seabios).
    
    This helps to test combined efi + pcbios disks as these can be
    bootet with both efi and pc bios.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- testsuite/start_vm.py
+++ testsuite/start_vm.py
@@ -21,7 +21,7 @@ def get_bitbake_var(output, var):
             ret = line.split('"')[1]
     return ret
 
-def format_qemu_cmdline(arch, build, distro, out, pid):
+def format_qemu_cmdline(arch, build, distro, out, pid, enforce_pcbios=False):
     bb_output = get_bitbake_env(arch, distro).decode()
 
     rootfs_image = ''
@@ -66,6 +66,9 @@ def format_qemu_cmdline(arch, build, distro, out, pid):
         extra_args.extend(['-pidfile', pid])
 
     qemu_disk_args = qemu_disk_args.replace('##ROOTFS_IMAGE##', deploy_dir_image + '/' + rootfs_image).split()
+    if enforce_pcbios and '-bios' in qemu_disk_args:
+        bios_idx = qemu_disk_args.index('-bios')
+        del qemu_disk_args[bios_idx : bios_idx+2]
 
     cmd = ['qemu-system-' + qemu_arch, '-m', '1024M']
 
@@ -80,8 +83,8 @@ def format_qemu_cmdline(arch, build, distro, out, pid):
 
     return cmd
 
-def start_qemu(arch, build, distro, out, pid):
-    cmdline = format_qemu_cmdline(arch, build, distro, out, pid)
+def start_qemu(arch, build, distro, out, pid, enforce_pcbios):
+    cmdline = format_qemu_cmdline(arch, build, distro, out, pid, enforce_pcbios)
     cmdline.insert(1, '-nographic')
 
     print(cmdline)
@@ -94,6 +97,7 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--distro', choices=['jessie', 'stretch', 'buster', 'bullseye', 'bookworm'], help='set isar Debian distribution.', default='stretch')
     parser.add_argument('-o', '--out', help='Route QEMU console output to specified file.')
     parser.add_argument('-p', '--pid', help='Store QEMU pid to specified file.')
+    parser.add_argument('--pcbios', action="store_true", help='remove any bios options to enforce use of pc bios')
     args = parser.parse_args()
 
-    start_qemu(args.arch, args.build, args.distro, args.out, args.pid)
+    start_qemu(args.arch, args.build, args.distro, args.out, args.pid, args.pcbios)
