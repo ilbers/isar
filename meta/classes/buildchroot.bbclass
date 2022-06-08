ilbers/isar

commit 4e1dacccfba62d006521df828517200fd949763e

Avoid sharing of /dev/shm from the build context
By bind-mounting complete /dev into the various chroots, we also share
the host instance of /dev/shm between them. If some package installation
should actually make use of that tmpfs instance, it may find content of
others there. That is at least not desirable, in few cases even
problematic (sysrepo package uses it during postinst, and this causes
troubles when multiple images are built in parallel).

This decouples all instances by mounting new instances over the
bind-mounted ones.

While at it, it switches the recursive bind-mounting of /dev to
explicit one. /dev/shm then becomes the only sub-mount. This is assumed
to be sufficient for the given use cases.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/buildchroot.bbclass
@@ -42,8 +42,8 @@ buildchroot_do_mounts() {
                 mount --bind '${CCACHE_DIR}' '${BUILDCHROOT_DIR}/ccache'
         fi
         mountpoint -q '${BUILDCHROOT_DIR}/dev' ||
-            mount --rbind /dev '${BUILDCHROOT_DIR}/dev'
-        mount --make-rslave '${BUILDCHROOT_DIR}/dev'
+            ( mount --bind /dev '${BUILDCHROOT_DIR}/dev' &&
+              mount -t tmpfs none '${BUILDCHROOT_DIR}/dev/shm' )
         mountpoint -q '${BUILDCHROOT_DIR}/proc' ||
             mount -t proc none '${BUILDCHROOT_DIR}/proc'
         mountpoint -q '${BUILDCHROOT_DIR}/sys' ||

meta/classes/rootfs.bbclass
@@ -34,8 +34,8 @@ rootfs_do_mounts() {
     sudo -s <<'EOSUDO'
         set -e
         mountpoint -q '${ROOTFSDIR}/dev' || \
-            mount --rbind /dev '${ROOTFSDIR}/dev'
-        mount --make-rslave '${ROOTFSDIR}/dev'
+            ( mount --bind /dev '${ROOTFSDIR}/dev' &&
+              mount -t tmpfs none '${ROOTFSDIR}/dev/shm' )
         mountpoint -q '${ROOTFSDIR}/proc' || \
             mount -t proc none '${ROOTFSDIR}/proc'
         mountpoint -q '${ROOTFSDIR}/sys' || \

meta/recipes-core/isar-bootstrap/isar-bootstrap.inc
@@ -360,8 +360,8 @@ do_bootstrap() {
         "${ROOTFSDIR}/chroot-setup.sh" "setup" "${ROOTFSDIR}"
 
         # update APT
-        mount --rbind /dev ${ROOTFSDIR}/dev
-        mount --make-rslave ${ROOTFSDIR}/dev
+        mount --bind /dev ${ROOTFSDIR}/dev
+        mount -t tmpfs none "${ROOTFSDIR}/dev/shm"
         mount -t proc none ${ROOTFSDIR}/proc
         mount --rbind /sys ${ROOTFSDIR}/sys
         mount --make-rslave ${ROOTFSDIR}/sys
@@ -381,6 +381,7 @@ do_bootstrap() {
         chroot "${ROOTFSDIR}" /usr/bin/apt-get dist-upgrade -y \
                                 -o Debug::pkgProblemResolver=yes
 
+        umount -l "${ROOTFSDIR}/dev/shm"
         umount -l "${ROOTFSDIR}/dev"
         umount -l "${ROOTFSDIR}/proc"
         umount -l "${ROOTFSDIR}/sys"

commit 5d04f390f6719b0cc8c0f8fed204cbd972b24f6e

fix: mount pts into rootfs
This fixes a regression introduced in 4e1dacc.
Pseudo ttys might be required during package building
as well as package installation
(e.g. to make expect work).

The patch mounts /dev/pts from the host.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/classes/buildchroot.bbclass
@@ -43,7 +43,8 @@ buildchroot_do_mounts() {
         fi
         mountpoint -q '${BUILDCHROOT_DIR}/dev' ||
             ( mount --bind /dev '${BUILDCHROOT_DIR}/dev' &&
-              mount -t tmpfs none '${BUILDCHROOT_DIR}/dev/shm' )
+              mount -t tmpfs none '${BUILDCHROOT_DIR}/dev/shm' &&
+              mount --bind /dev/pts '${BUILDCHROOT_DIR}/dev/pts' )
         mountpoint -q '${BUILDCHROOT_DIR}/proc' ||
             mount -t proc none '${BUILDCHROOT_DIR}/proc'
         mountpoint -q '${BUILDCHROOT_DIR}/sys' ||

meta/classes/rootfs.bbclass
@@ -35,7 +35,8 @@ rootfs_do_mounts() {
         set -e
         mountpoint -q '${ROOTFSDIR}/dev' || \
             ( mount --bind /dev '${ROOTFSDIR}/dev' &&
-              mount -t tmpfs none '${ROOTFSDIR}/dev/shm' )
+              mount -t tmpfs none '${ROOTFSDIR}/dev/shm' &&
+              mount --bind /dev/pts '${ROOTFSDIR}/dev/pts' )
         mountpoint -q '${ROOTFSDIR}/proc' || \
             mount -t proc none '${ROOTFSDIR}/proc'
         mountpoint -q '${ROOTFSDIR}/sys' || \

meta/recipes-core/isar-bootstrap/isar-bootstrap.inc
@@ -370,6 +370,7 @@ do_bootstrap() {
 
         # update APT
         mount --bind /dev ${ROOTFSDIR}/dev
+        mount --bind /dev/pts ${ROOTFSDIR}/dev/pts
         mount -t tmpfs none "${ROOTFSDIR}/dev/shm"
         mount -t proc none ${ROOTFSDIR}/proc
         mount --rbind /sys ${ROOTFSDIR}/sys
@@ -391,6 +392,7 @@ do_bootstrap() {
                                 -o Debug::pkgProblemResolver=yes
 
         umount -l "${ROOTFSDIR}/dev/shm"
+        umount -l "${ROOTFSDIR}/dev/pts"
         umount -l "${ROOTFSDIR}/dev"
         umount -l "${ROOTFSDIR}/proc"
         umount -l "${ROOTFSDIR}/sys"