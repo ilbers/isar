ilbers/isar

commit 342a751ef54cafa8d2f5513e532f090eaf5fdad8

expand-on-first-boot: Resolve errors in helper script
Missing quotes broke the -n test, and as we are at it, also resolve the
shellcheck warnings and consolidate over 'find' for retrieving
ROOT_DEV_SLAVE.

Fixes: 15214487e19e ("expand-on-first-boot: Add support for devicemapper")
Reported-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/recipes-support/expand-on-first-boot/files/expand-last-partition.sh
@@ -11,8 +11,8 @@ set -e
 
 ROOT_DEV="$(findmnt / -o source -n)"
 ROOT_DEV_NAME=${ROOT_DEV##*/}
-ROOT_DEV_SLAVE=$(ls -d /sys/block/${ROOT_DEV_NAME}/slaves/* 2>/dev/null | head -1)
-if [ -n ${ROOT_DEV_SLAVE} ]; then
+ROOT_DEV_SLAVE=$(find /sys/block/"${ROOT_DEV_NAME}"/slaves -mindepth 1 -print -quit 2>/dev/null)
+if [ -n "${ROOT_DEV_SLAVE}" ]; then
 	ROOT_DEV=/dev/${ROOT_DEV_SLAVE##*/}
 fi
 

commit c806c9d433e9d7ea54ff346f6cacb34f2863efa5

expand-on-first-boot: Fix early exit with error in helper script
Changes in 342a751e introduced a regression when error from 'find'
makes helper script early exit (with -1 error code) just after an
attempt to assign ROOT_DEV_SLAVE variable. So, the last partition
remained not resized.

Masking possible error from 'find' doesn't break the logic and allows
to continue the script execution.

Signed-off-by: Uladzimir Bely <ubely@ilbers.de>

meta/recipes-support/expand-on-first-boot/files/expand-last-partition.sh
@@ -11,7 +11,7 @@ set -e
 
 ROOT_DEV="$(findmnt / -o source -n)"
 ROOT_DEV_NAME=${ROOT_DEV##*/}
-ROOT_DEV_SLAVE=$(find /sys/block/"${ROOT_DEV_NAME}"/slaves -mindepth 1 -print -quit 2>/dev/null)
+ROOT_DEV_SLAVE=$(find /sys/block/"${ROOT_DEV_NAME}"/slaves -mindepth 1 -print -quit 2>/dev/null || true)
 if [ -n "${ROOT_DEV_SLAVE}" ]; then
 	ROOT_DEV=/dev/${ROOT_DEV_SLAVE##*/}
 fi