ilbers/isar

commit a298cccda57f45a31b7abdd339244d49a325602c

expand-on-first-boot: List fdisk as explicit dependency
Needed for bullseye where this dropped from util-linux.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/recipes-support/expand-on-first-boot/expand-on-first-boot_1.1.bb
@@ -9,7 +9,7 @@ inherit dpkg-raw
 
 DESCRIPTION = "This service grows the last partition to the full medium during first boot"
 
-DEBIAN_DEPENDS = "systemd, sed, grep, coreutils, mount, e2fsprogs, util-linux"
+DEBIAN_DEPENDS = "systemd, sed, grep, coreutils, mount, e2fsprogs, fdisk, util-linux"
 
 SRC_URI = " \
     file://expand-on-first-boot.service \

commit 91ae9058c576f10433ebbb5b1a3e6f60171cf438

expand-on-first-boot: fix runtime deps for debian stretch
In debian stretch sfdisk was part of util-linux and not the new package
fdisk. Change the DEBIAN_DEPENDS to allow installation on stretch again.

Fixes: a298cccda57f ("expand-on-first-boot: List fdisk as explicit dependency")
Signed-off-by: Henning Schild <henning.schild@siemens.com>

meta/recipes-support/expand-on-first-boot/expand-on-first-boot_1.1.bb
@@ -9,7 +9,7 @@ inherit dpkg-raw
 
 DESCRIPTION = "This service grows the last partition to the full medium during first boot"
 
-DEBIAN_DEPENDS = "systemd, sed, grep, coreutils, mount, e2fsprogs, fdisk, util-linux"
+DEBIAN_DEPENDS = "systemd, sed, grep, coreutils, mount, e2fsprogs, fdisk (>=2.29.2-3) | util-linux (<2.29.2-3), util-linux"
 
 SRC_URI = " \
     file://expand-on-first-boot.service \