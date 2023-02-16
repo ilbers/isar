ilbers/isar

commit 924aa382ca2680acc7e3d043b0bfd88f41890f90

bitbake.conf: declare default XZ and ZSTD options
Put default compression options into bitbake.conf to allign with OE.

Signed-off-by: Anton Mikanovich <amikan@ilbers.de>

meta/classes/imagetypes.bbclass
@@ -98,10 +98,5 @@ IMAGE_CONVERSIONS = "gz xz"
 CONVERSION_CMD:gz = "${SUDO_CHROOT} sh -c 'gzip -f -9 -n -c --rsyncable ${IMAGE_FILE_CHROOT} > ${IMAGE_FILE_CHROOT}.gz'"
 CONVERSION_DEPS:gz = "gzip"
 
-XZ_MEMLIMIT ?= "50%"
-XZ_THREADS ?= "${@oe.utils.cpu_count(at_least=2)}"
-XZ_THREADS[vardepvalue] = "1"
-XZ_OPTIONS ?= "--memlimit=${XZ_MEMLIMIT} --threads=${XZ_THREADS}"
-XZ_OPTIONS[vardepsexclude] += "XZ_MEMLIMIT XZ_THREADS"
 CONVERSION_CMD:xz = "${SUDO_CHROOT} sh -c 'xz -c ${XZ_OPTIONS} ${IMAGE_FILE_CHROOT} > ${IMAGE_FILE_CHROOT}.xz'"
 CONVERSION_DEPS:xz = "xz-utils"

meta/conf/bitbake.conf
@@ -130,6 +130,17 @@ BB_NUMBER_THREADS ?= "${@bb.utils.cpu_count()}"
 # Default to setting automatically based on cpu count
 PARALLEL_MAKE ?= "-j ${@bb.utils.cpu_count()}"
 
+# Default parallelism and resource usage for xz
+XZ_MEMLIMIT ?= "50%"
+XZ_THREADS ?= "${@oe.utils.cpu_count(at_least=2)}"
+XZ_THREADS[vardepvalue] = "1"
+XZ_DEFAULTS ?= "--memlimit=${XZ_MEMLIMIT} --threads=${XZ_THREADS}"
+XZ_DEFAULTS[vardepsexclude] += "XZ_MEMLIMIT XZ_THREADS"
+
+# Default parallelism for zstd
+ZSTD_THREADS ?= "${@oe.utils.cpu_count(at_least=2)}"
+ZSTD_THREADS[vardepvalue] = "1"
+
 BBINCLUDELOGS ??= "yes"
 
 # Add event handlers for bitbake

commit 1f8520e2f5f0b8061c89de8730371d38bf1c133b

imagetypes XZ_OPTIONS replaced by XZ_DEFAULTS defined in bitbake.conf
imagetypes, bugfix

A previous patch moved XZ_ variables declarations into bitbake.conf from
the imagetypes class. In such patch the XZ_OPTIONS has been renamed in
XZ_DEFAULTS but the XZ_ command in imagetype class has not been updated.
This patch fixed this bug which decreases performance and alters the
expected behaviour.

Fixes: 924aa382ca26 ("bitbake.conf: declare default XZ and ZSTD options")

Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

meta/classes/imagetypes.bbclass
@@ -98,7 +98,7 @@ IMAGE_CONVERSIONS = "gz xz zst"
 CONVERSION_CMD:gz = "${SUDO_CHROOT} sh -c 'gzip -f -9 -n -c --rsyncable ${IMAGE_FILE_CHROOT} > ${IMAGE_FILE_CHROOT}.gz'"
 CONVERSION_DEPS:gz = "gzip"
 
-CONVERSION_CMD:xz = "${SUDO_CHROOT} sh -c 'xz -c ${XZ_OPTIONS} ${IMAGE_FILE_CHROOT} > ${IMAGE_FILE_CHROOT}.xz'"
+CONVERSION_CMD:xz = "${SUDO_CHROOT} sh -c 'xz -c ${XZ_DEFAULTS} ${IMAGE_FILE_CHROOT} > ${IMAGE_FILE_CHROOT}.xz'"
 CONVERSION_DEPS:xz = "xz-utils"
 
 CONVERSION_CMD:zst = "${SUDO_CHROOT} sh -c 'zstd -c ${ZSTD_DEFAULTS} ${IMAGE_FILE_CHROOT} > ${IMAGE_FILE_CHROOT}.zst'"