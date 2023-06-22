ilbers/isar

commit 961a56828ca3043c01ff521f293f44fd2201667e

classes/initramfs: Print messages in case of a build error
This patch eases the fault analysis in case of a broken
initramfs generation.

This prints the messages from 'update-initramfs' instead of:

```
Available versions:
Nothing to do, exiting.
ERROR: No initramfs was found after generation!
```

Signed-off-by: Quirin Gylstorff <quirin.gylstorff@siemens.com>

meta/classes/initramfs.bbclass
@@ -43,11 +43,16 @@ do_generate_initramfs() {
         export SOURCE_DATE_EPOCH="${SOURCE_DATE_EPOCH}"
     fi
 
-    sudo -E chroot "${INITRAMFS_ROOTFS}" \
-        update-initramfs -u -v
+    sudo -E chroot "${INITRAMFS_ROOTFS}" sh -c '\
+        export kernel_version=$(basename /boot/vmlinux* | cut -d'-' -f2-); \
+        if [ -n "$kernel_version" ]; then \
+          update-initramfs -u -v -k "$kernel_version"; \
+        else \
+          update-initramfs -u -v ;  \
+        fi'
 
     if [ ! -e "${INITRAMFS_ROOTFS}/initrd.img" ]; then
-        die "No initramfs was found after generation!"
+        bberror "No initramfs was found after generation!"
     fi
     cp ${INITRAMFS_ROOTFS}/initrd.img ${DEPLOYDIR}/${INITRAMFS_IMAGE_NAME}
 }

commit 478f3c324f50eca0d082cd1c8f23ef50e28071fb

initramfs: Fix detection of kernel image
This can also be vmlinuz-.... Broken like this when using the class for
x86 e.g.:

Log data follows:
| DEBUG: Executing python function sstate_task_prefunc
| DEBUG: Python function sstate_task_prefunc finished
| DEBUG: Executing shell function do_generate_initramfs
| update-initramfs: Generating /boot/initrd.img-vmlinux*
| W: No zstd in /usr/bin:/sbin:/bin, using gzip
| grep: /boot/config-vmlinux*: No such file or directory
| E: gzip compression (CONFIG_RD_GZIP) not supported by kernel
| update-initramfs: failed for /boot/initrd.img-vmlinux* with 1.
| WARNING: exit code 1 from a shell command.
ERROR: Task (/build/../work/recipes-initramfs/cip-core-initramfs/cip-core-initramfs.bb:do_generate_initramfs) failed with exit code '1'

Reported-by: Venkata Pyla <venkata.pyla@toshiba-tsip.com>
Fixes: 961a56828ca3 ("classes/initramfs: Print messages in case of a build error")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/initramfs.bbclass
@@ -44,7 +44,7 @@ do_generate_initramfs() {
     fi
 
     sudo -E chroot "${INITRAMFS_ROOTFS}" sh -c '\
-        export kernel_version=$(basename /boot/vmlinux* | cut -d'-' -f2-); \
+        export kernel_version=$(basename /boot/vmlinu[xz]* | cut -d'-' -f2-); \
         if [ -n "$kernel_version" ]; then \
           update-initramfs -u -v -k "$kernel_version"; \
         else \