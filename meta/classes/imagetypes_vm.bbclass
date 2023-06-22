ilbers/isar

commit 3ac61204e1ed3d1c677a1a89a4448b36b60e6316

replace custom OVA logic with imagetype logic
This patch refactors the imagetypes_vm class for virtualbox and vmware
images. The existing manual implementation does not work anymore when
referencing ova config files across layers. Instead, we now use the
imagetypes logic, which avoids the problematic changes of
FILESEXTRAPATHS and SRC_URI.

Acked-by: Adriaan Schmidt <adriaan.schmidt@siemens.com>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/classes/imagetypes_vm.bbclass
@@ -6,11 +6,8 @@
 
 inherit buildchroot
 
-USING_OVA = "${@bb.utils.contains('IMAGE_BASETYPES', 'ova', '1', '0', d)}"
-
-FILESEXTRAPATHS:prepend := "${LAYERDIR_core}/classes/vm-img:"
 OVF_TEMPLATE_FILE ?= "vm-img-virtualbox.ovf.tmpl"
-SRC_URI += "${@'file://${OVF_TEMPLATE_FILE}' if d.getVar('USING_OVA') == '1' else ''}"
+IMAGE_SRC_URI:ova = "file://${OVF_TEMPLATE_FILE}"
 
 IMAGE_TYPEDEP:ova = "wic"
 IMAGER_INSTALL:ova += "qemu-utils gawk uuid-runtime"
@@ -67,8 +64,8 @@ OVA_VARS = "OVA_NAME OVA_MEMORY OVA_NUMBER_OF_CPU OVA_VRAM \
             OVA_FIRMWARE OVA_ACPI OVA_3D_ACCEL \
             OVA_SHA_ALG VIRTUAL_MACHINE_IMAGE_FILE"
 
-TEMPLATE_FILES += "${@'${OVF_TEMPLATE_FILE}' if d.getVar('USING_OVA') == '1' else ''}"
-TEMPLATE_VARS += "${OVA_VARS}"
+IMAGE_TEMPLATE_FILES:ova = "${OVF_TEMPLATE_FILE}"
+IMAGE_TEMPLATE_VARS:ova = "${OVA_VARS}"
 
 do_image_ova[prefuncs] += "convert_wic"
 IMAGE_CMD:ova() {

commit cccff78177329983099b31ec56ad820afb446341

vm-img: move templates from meta-isar to meta again
The vm image class needs these templates to work in cases where people
do not have their own copies in their layers. Having them in meta-isar
means that layers using the class with the default templates would have
to use that example layer as well.

Fixes: 3ac61204e1ed ("replace custom OVA logic with imagetype logic")
Signed-off-by: Henning Schild <henning.schild@siemens.com>
Reported-by: Clara Kowalsky <clara.kowalsky@siemens.com>

meta/classes/imagetypes_vm.bbclass
@@ -6,6 +6,7 @@
 
 inherit buildchroot
 
+FILESEXTRAPATHS:prepend = "${LAYERDIR_core}/recipes-core/images:"
 OVF_TEMPLATE_FILE ?= "vm-img-virtualbox.ovf.tmpl"
 IMAGE_SRC_URI:ova = "file://${OVF_TEMPLATE_FILE}"
 