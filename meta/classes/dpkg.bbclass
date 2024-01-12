ilbers/isar

commit b382dbf434d964a3ce8eec43ac96db3527363f54

dpkg: Account for empty local apt cache after package build
A build dependency-free package combined with sstate caching and cleared
deb cache can trigger

| cp -Ln --no-preserve=owner /var/cache/apt/archives/*.deb -t /home/builder/example-raw/rootfs/var/cache/apt/archives/
| --------------------------------------------------------------------------------------------------------------------
|
| cp: cannot stat '/var/cache/apt/archives/*.deb': No such file or directory
|
| E: Command 'cp -Ln --no-preserve=owner /var/cache/apt/archives/*.deb -t /home/builder/example-raw/rootfs/var/cache/apt/archives/' failed to run.

Account for that.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
Acked-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/classes/dpkg.bbclass
@@ -109,7 +109,7 @@ dpkg_runbuild() {
         --chroot-setup-commands="mkdir -p ${deb_dir}" \
         --chroot-setup-commands="ln -sf ${ext_deb_dir}/*.deb -t ${deb_dir}/" \
         --finished-build-commands="rm -f ${deb_dir}/sbuild-build-depends-main-dummy_*.deb" \
-        --finished-build-commands="cp -Ln --no-preserve=owner ${deb_dir}/*.deb -t ${ext_deb_dir}/" \
+        --finished-build-commands="[ -z "$(find ${deb_dir} -maxdepth 1 -name '*.deb' -print -quit)" ] || cp -Ln --no-preserve=owner ${deb_dir}/*.deb -t ${ext_deb_dir}/" \
         --finished-build-commands="cp /var/log/dpkg.log ${ext_root}/dpkg_partial.log" \
         --debbuildopts="--source-option=-I" \
         --build-dir=${WORKDIR} --dist="isar" ${DSC_FILE}

commit 1c509852c5a724a2c3ba401108d95fb1bfa36f90

dpkg: Prevent early expansion of shell command
So far, the 'find' command in the sub-shell is already executed before
the sbuild call, rather than after it. Fix this by escaping the $ as
well as the quotes.

Reported-by: Uladzimir Bely <ubely@ilbers.de>
Fixes: b382dbf434d9 ("dpkg: Account for empty local apt cache after package build")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/dpkg.bbclass
@@ -121,7 +121,7 @@ dpkg_runbuild() {
         --chroot-setup-commands="mkdir -p ${deb_dir}" \
         --chroot-setup-commands="ln -sf ${ext_deb_dir}/*.deb -t ${deb_dir}/" \
         --finished-build-commands="rm -f ${deb_dir}/sbuild-build-depends-main-dummy_*.deb" \
-        --finished-build-commands="[ -z "$(find ${deb_dir} -maxdepth 1 -name '*.deb' -print -quit)" ] || cp ${CP_FLAGS} ${deb_dir}/*.deb -t ${ext_deb_dir}/" \
+        --finished-build-commands="[ -z \"\$(find ${deb_dir} -maxdepth 1 -name '*.deb' -print -quit)\" ] || cp ${CP_FLAGS} ${deb_dir}/*.deb -t ${ext_deb_dir}/" \
         --finished-build-commands="cp /var/log/dpkg.log ${ext_root}/dpkg_partial.log" \
         --debbuildopts="--source-option=-I" \
         --build-dir=${WORKDIR} --dist="isar" ${DSC_FILE}