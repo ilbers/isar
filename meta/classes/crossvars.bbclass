ilbers/isar

commit 95427cc8a760f4dc9244bf587bc7f9989a24ccd6

crossvars: Adjust logic to account for -native package builds in non-cross setups
When selecting a -native version of a package while cross-building is
disabled for it, we will use the target build env instead. That may
work, just slowly, or even fail miserably.

Extend the build env selection logic to take PACKAGE_ARCH into account
as well. It defaults to DISTRO_ARCH for non-package recipe, thus should
works fine for those as well.

Reported-by: Cedric Hombourger <cedric.hombourger@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/crossvars.bbclass
@@ -16,16 +16,22 @@ python __anonymous() {
     flavor_suffix = ('-' + flavor) if flavor else ''
 
     distro_arch = d.getVar('DISTRO_ARCH')
-    if mode == "0" or d.getVar('HOST_ARCH') == distro_arch or distro_arch == None:
+    compat_arch = d.getVar('COMPAT_DISTRO_ARCH')
+    host_arch = d.getVar('HOST_ARCH')
+    package_arch = d.getVar('PACKAGE_ARCH')
+
+    if distro_arch != host_arch and \
+        (package_arch == host_arch or \
+         (package_arch in [distro_arch, compat_arch] and mode == "1")):
+        d.setVar('BUILD_ARCH', host_arch)
+        schroot_dir = d.getVar('SCHROOT_HOST_DIR', False)
+        sbuild_dep = "sbuild-chroot-host" + flavor_suffix + ":do_build"
+        sdk_toolchain = "crossbuild-essential-" + distro_arch
+    else:
         d.setVar('BUILD_ARCH', distro_arch)
         schroot_dir = d.getVar('SCHROOT_TARGET_DIR', False)
         sbuild_dep = "sbuild-chroot-target" + flavor_suffix + ":do_build"
         sdk_toolchain = "build-essential"
-    else:
-        d.setVar('BUILD_ARCH', d.getVar('HOST_ARCH'))
-        schroot_dir = d.getVar('SCHROOT_HOST_DIR', False)
-        sbuild_dep = "sbuild-chroot-host" + flavor_suffix + ":do_build"
-        sdk_toolchain = "crossbuild-essential-" + distro_arch
     d.setVar('SCHROOT_DIR', schroot_dir + flavor_suffix)
     d.setVar('SCHROOT_DEP', sbuild_dep)
     if isar_can_build_compat(d):

commit 33b00ca7821b6d3cbd845cbc0ff1869fd209ef47

crossvars: Account for PACKAGE_ARCH = "${BUILD_ARCH}"
95427cc8 forgot about the case that PACKAGE_ARCH is to be derived from
BUILD_ARCH - while that is considering PACKAGE_ARCH before setting
BUILD_ARCH at all. Account for that case by adding the unresolved value
of PACKAGE_ARCH into the cross-build case because that is what the
statement is about: Set PACKAGE_ARCH to HOST_ARCH only if cross-building
is enabled for a package.

Fixes: 95427cc8a760 ("crossvars: Adjust logic to account for -native package builds in non-cross setups")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/crossvars.bbclass
@@ -22,7 +22,7 @@ python __anonymous() {
 
     if distro_arch != host_arch and \
         (package_arch == host_arch or \
-         (package_arch in [distro_arch, compat_arch] and mode == "1")):
+         (package_arch in [distro_arch, compat_arch, '${BUILD_ARCH}'] and mode == "1")):
         d.setVar('BUILD_ARCH', host_arch)
         schroot_dir = d.getVar('SCHROOT_HOST_DIR', False)
         sbuild_dep = "sbuild-chroot-host" + flavor_suffix + ":do_build"