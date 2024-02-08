ilbers/isar

commit a0b293c53d67682233994437080fe55021b79896

dpkg-runbuild: start with clean apt-cache
The global apt-cache is imported during dpkg-runbuild and cleaned once
the function completes. This works properly on successfull builds.
However, in case of a build failure, the apt-cache dir is not cleaned
and re-read on the next build, leading to unstable builds in case
erronous data was added to the cache.

This patch adds the local apt-cache dir to the cleandirs and by that
cleans it before the function is executed.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/classes/dpkg.bbclass
@@ -34,6 +34,7 @@ CP_FLAGS ?= "-Ln --no-preserve=owner"
 CP_FLAGS:sid ?= "-L --update=none --no-preserve=owner"
 
 # Build package from sources using build script
+dpkg_runbuild[cleandirs] += "${WORKDIR}/rootfs"
 dpkg_runbuild[vardepsexclude] += "${SBUILD_PASSTHROUGH_ADDITIONS}"
 dpkg_runbuild() {
     E="${@ isar_export_proxies(d)}"

commit 94ab67581ffdd96fd225772b0fd921eaaad86ff6

fix: clean apt-cache dirs in dpkg_runbuild as root
This patch fixes a regression introduced in a0b293c5: The cleaning
of the rootfs dir before dpkg_runbuild needs to be performed as root, as
this dir might contain data which cannot be deleted by the default user.

Reported-by: Jan Kiszka <jan.kiszka@siemens.com>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/classes/dpkg.bbclass
@@ -34,7 +34,7 @@ CP_FLAGS ?= "-Ln --no-preserve=owner"
 CP_FLAGS:sid ?= "-L --update=none --no-preserve=owner"
 
 # Build package from sources using build script
-dpkg_runbuild[cleandirs] += "${WORKDIR}/rootfs"
+dpkg_runbuild[root_cleandirs] += "${WORKDIR}/rootfs"
 dpkg_runbuild[vardepsexclude] += "${SBUILD_PASSTHROUGH_ADDITIONS}"
 dpkg_runbuild() {
     E="${@ isar_export_proxies(d)}"