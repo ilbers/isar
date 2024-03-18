ilbers/isar

commit f50237eb059c14f39ae3f3856f72d2aafbee4b40

multiarch: Fix SRC_URI:remove ignoring
Setting SRC_URI right after getVar with expand=False results in loosing
SRC_URI:remove values. Rebuild logic to use remove-append approach.

Signed-off-by: Anton Mikanovich <amikan@ilbers.de>

meta/classes/multiarch.bbclass
@@ -49,8 +49,9 @@ python multiarch_virtclass_handler() {
         vars = 'SRC_URI FILESPATH'.split()
         for var in vars:
             v = d.getVar(var, expand=False)
-            if v is not None:
-                d.setVar(var, v.replace('${PN}', '${BPN}'))
+            if v is not None and '${PN}' in v:
+                d.setVar(var + ':remove', v)
+                d.appendVar(var, v.replace('${PN}', '${BPN}'))
 
     # When building compat/native, the corresponding suffix needs to be
     # propagated to all bitbake dependency definitions.

commit bfd2f426f59afdfc18b12785df7bdba772dd2ec1

multiarch: Add missing space in BPN append
Adding values must be done with a leading space.

Fixes: f50237eb059c ("multiarch: Fix SRC_URI:remove ignoring")
Reported-by: Adriaan Schmidt <adriaan.schmidt@siemens.com>
Signed-off-by: Anton Mikanovich <amikan@ilbers.de>

meta/classes/multiarch.bbclass
@@ -51,7 +51,7 @@ python multiarch_virtclass_handler() {
             v = d.getVar(var, expand=False)
             if v is not None and '${PN}' in v:
                 d.setVar(var + ':remove', v)
-                d.appendVar(var, v.replace('${PN}', '${BPN}'))
+                d.appendVar(var, ' ' + v.replace('${PN}', '${BPN}'))
 
     # When building compat/native, the corresponding suffix needs to be
     # propagated to all bitbake dependency definitions.