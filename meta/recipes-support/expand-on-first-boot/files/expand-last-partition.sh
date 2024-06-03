ilbers/isar

commit 991d2e903930463be3362f14503283418d99d868

expand-on-first-boot: Preserve MBR on expansion of GPT disks
In order to keep potential hybrid partition setups intact, preserve the
legacy MBR when a GPT disk is being expanded. This is needed because we
rewrite the partitioning via sfdisk which overwrites the MBR. In the
hypbrid setup, the legacy MBR plays no role regarding the expanded
partition, thus can be simply left untouched.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/recipes-support/expand-on-first-boot/files/expand-last-partition.sh
@@ -38,6 +38,11 @@ if [ "$DISK_SIZE" -lt "$MINIMAL_SIZE" ]; then
 	exit 0
 fi
 
+IS_GPT="$(sfdisk -d "${BOOT_DEV}" 2>/dev/null | grep -q "label: gpt" && echo 1)"
+if [ "$IS_GPT" = "1" ]; then
+	dd if="${BOOT_DEV}" of=/dev/shm/__mbr__.bak count=1
+fi
+
 LAST_PART="$(sfdisk -d "${BOOT_DEV}" 2>/dev/null | tail -1 | cut -d ' ' -f 1)"
 
 # Transform the partition table as follows:
@@ -53,6 +58,11 @@ sfdisk -d "${BOOT_DEV}" 2>/dev/null | \
 	sed 's|^\(.*, \)size=[^,]*, \(type=[f5]\)$|\1\2|' | \
 	sed 's|^\('"${LAST_PART}"' .*, \)size=[^,]*, |\1|' | \
 	sfdisk --force "${BOOT_DEV}"
+
+if [ "$IS_GPT" = "1" ]; then
+	dd if=/dev/shm/__mbr__.bak of="${BOOT_DEV}"
+	rm /dev/shm/__mbr__.bak
+fi
 
 # Inform the kernel about the partitioning change
 partx -u "${LAST_PART}"

commit 38751e7b0ed39d74a0034acafa90f1ff81696457

expand-on-first-boot: Fix "label: gpt" detection
Commit 991d2e9 introduced the issue when the whole script early exits
(due to "set -x") in case grep doesn't find the pattern and produces
an error code.

In this case last partition remains non-expanded.

Fix this by masking error with "echo 0".

Signed-off-by: Uladzimir Bely <ubely@ilbers.de>

meta/recipes-support/expand-on-first-boot/files/expand-last-partition.sh
@@ -41,7 +41,7 @@ if [ "$DISK_SIZE" -lt "$MINIMAL_SIZE" ]; then
 	exit 0
 fi
 
-IS_GPT="$(sfdisk -d "${BOOT_DEV}" 2>/dev/null | grep -q "label: gpt" && echo 1)"
+IS_GPT="$(sfdisk -d "${BOOT_DEV}" 2>/dev/null | grep -q "label: gpt" && echo 1 || echo 0)"
 if [ "$IS_GPT" = "1" ]; then
 	dd if="${BOOT_DEV}" of=/dev/shm/__mbr__.bak count=1
 fi