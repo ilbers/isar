ilbers/isar

commit 0816ae6e97d15712f6b8eb873311464263f5df59

dpkg-raw: change DPKG_ARCH to all
Packages created by dpkg-raw are really for configuration and data files
and therefore architecture independent. DPKG_ARCH is now set to "all" and
DEBIAN_MULTI_ARCH will default to "foreign" unless DPKG_ARCH is changed
(it would then be reset to "no").

Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

meta/classes/dpkg-raw.bbclass
@@ -7,6 +7,10 @@ inherit dpkg
 
 D = "${WORKDIR}/image"
 
+# Create a binary-indep package
+DPKG_ARCH = "all"
+DEBIAN_MULTI_ARCH = "${@ 'foreign' if '${DPKG_ARCH}' == 'all' else 'no' }"
+
 # Populate folder that will be picked up as package
 do_install() {
 	bbnote "Put your files for this package in $""{D}"

commit 22805a4cea35e42c634a80bc978c0546b5007a16

dpkg-raw: use weak assignments for DPKG_ARCH and DEBIAN_MULTI_ARCH
Fixes: 0816ae6e
Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

meta/classes/dpkg-raw.bbclass
@@ -7,9 +7,9 @@ inherit dpkg
 
 D = "${WORKDIR}/image"
 
-# Create a binary-indep package
-DPKG_ARCH = "all"
-DEBIAN_MULTI_ARCH = "${@ 'foreign' if '${DPKG_ARCH}' == 'all' else 'no' }"
+# Default to creating a binary-indep package
+DPKG_ARCH ??= "all"
+DEBIAN_MULTI_ARCH ??= "${@ 'foreign' if '${DPKG_ARCH}' == 'all' else 'no' }"
 
 # Populate folder that will be picked up as package
 do_install() {