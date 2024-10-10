ilbers/isar

commit 165519a7b3147448d5846aaeed7af8222143a7c7

sudo: Fail on the first error
The code execution inside sudo section should be stopped on the first
command failure because the next commands can cause incorrect behavior.

Signed-off-by: Anton Mikanovich <amikan@ilbers.de>

meta/classes/image.bbclass
@@ -189,6 +189,7 @@ addtask deploy before do_build after do_image
 
 do_rootfs_finalize() {
     sudo -s <<'EOSUDO'
+        set -e
         test -e "${ROOTFSDIR}/chroot-setup.sh" && \
             "${ROOTFSDIR}/chroot-setup.sh" "cleanup" "${ROOTFSDIR}"
         rm -f "${ROOTFSDIR}/chroot-setup.sh"

meta/classes/deb-dl-dir.bbclass
@@ -22,6 +22,7 @@ is_not_part_of_current_build() {
 
 debsrc_do_mounts() {
     sudo -s <<EOSUDO
+    set -e
     mkdir -p "${1}/deb-src"
     mountpoint -q "${1}/deb-src" || \
     mount --bind "${DEBSRCDIR}" "${1}/deb-src"
@@ -30,6 +31,7 @@ EOSUDO
 
 debsrc_undo_mounts() {
     sudo -s <<EOSUDO
+    set -e
     mkdir -p "${1}/deb-src"
     mountpoint -q "${1}/deb-src" && \
     umount -l "${1}/deb-src"

meta/classes/image-locales-extension.bbclass
@@ -59,6 +59,7 @@ __EOF__
 
     # Install configuration into image:
     sudo -E -s <<'EOSUDO'
+        set -e
         cat '${WORKDIR}/locale.gen' >> '${ROOTFSDIR}/etc/locale.gen'
         cat '${WORKDIR}/locale.default' > '${ROOTFSDIR}/etc/default/locale'
         cat '${WORKDIR}/locale.nopurge' > '${ROOTFSDIR}/etc/locale.nopurge'

meta/classes/rootfs.bbclass
@@ -30,6 +30,7 @@ export LC_ALL = "C"
 rootfs_do_mounts[weight] = "3"
 rootfs_do_mounts() {
     sudo -s <<'EOSUDO'
+        set -e
         mountpoint -q '${ROOTFSDIR}/dev' || \
             mount --rbind /dev '${ROOTFSDIR}/dev'
         mount --make-rslave '${ROOTFSDIR}/dev'
@@ -80,6 +81,7 @@ ROOTFS_CONFIGURE_COMMAND += "rootfs_configure_isar_apt"
 rootfs_configure_isar_apt[weight] = "2"
 rootfs_configure_isar_apt() {
     sudo -s <<'EOSUDO'
+    set -e
 
     mkdir -p '${ROOTFSDIR}/etc/apt/sources.list.d'
     echo 'deb [trusted=yes] file:///isar-apt ${DEBDISTRONAME} main' > \

meta/classes/vm-img.bbclass
@@ -95,6 +95,7 @@ do_create_ova() {
     image_do_mounts
 
     sudo -Es chroot --userspec=$( id -u ):$( id -g ) ${BUILDCHROOT_DIR} <<'EOSUDO'
+        set -e
         export DISK_SIZE_BYTES=$(qemu-img info -f vmdk "${VIRTUAL_MACHINE_DISK}" \
                                  | gawk 'match($0, /^virtual size:.*\(([0-9]+) bytes\)/, a) {print a[1]}')
         export DISK_UUID=$(uuidgen)

meta/classes/wic-img.bbclass
@@ -156,6 +156,7 @@ wic_do_mounts() {
     buildchroot_do_mounts
     sudo -s <<'EOSUDO'
         ( flock 9
+        set -e
         for dir in ${BBLAYERS} ${STAGING_DIR} ${SCRIPTSDIR} ${BITBAKEDIR}; do
             mkdir -p ${BUILDCHROOT_DIR}/$dir
             if ! mountpoint ${BUILDCHROOT_DIR}/$dir >/dev/null 2>&1; then

meta/recipes-devtools/buildchroot/buildchroot.inc
@@ -44,6 +44,7 @@ BUILDCHROOT_PREINSTALL_COMMON = " \
 
 rootfs_do_mounts_append() {
     sudo -s <<'EOSUDO'
+    set -e
     mkdir -p '${BUILDCHROOT_DIR}/downloads'
     mountpoint -q '${BUILDCHROOT_DIR}/downloads' || \
         mount --bind '${DL_DIR}' '${BUILDCHROOT_DIR}/downloads'

commit c4edea6be50c40e1b05c27ca091d1cd96a218941

image: Avoid breaking the build when mounts are no longer present on umount
This does not seem to trigger yet because we always have those
mountpoints present. But if that is no longer the case, we will bail out
when mountpoint fails due to the set -e.

Fixes: 165519a7b314 ("sudo: Fail on the first error")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/image.bbclass
@@ -418,24 +418,31 @@ do_rootfs_finalize() {
                 -maxdepth 1 -name 'qemu-*-static' -type f -delete
         fi
 
-        mountpoint -q '${ROOTFSDIR}/isar-apt' && \
-            umount '${ROOTFSDIR}/isar-apt' && \
+        if mountpoint -q '${ROOTFSDIR}/isar-apt'; then
+            umount '${ROOTFSDIR}/isar-apt'
             rmdir --ignore-fail-on-non-empty ${ROOTFSDIR}/isar-apt
+        fi
 
-        mountpoint -q '${ROOTFSDIR}/base-apt' && \
-            umount '${ROOTFSDIR}/base-apt' && \
+        if mountpoint -q '${ROOTFSDIR}/base-apt'; then
+            umount '${ROOTFSDIR}/base-apt'
             rmdir --ignore-fail-on-non-empty ${ROOTFSDIR}/base-apt
+        fi
 
-        mountpoint -q '${ROOTFSDIR}/dev/pts' && \
+        if mountpoint -q '${ROOTFSDIR}/dev/pts'; then
             umount '${ROOTFSDIR}/dev/pts'
-        mountpoint -q '${ROOTFSDIR}/dev/shm' && \
+        fi
+        if mountpoint -q '${ROOTFSDIR}/dev/shm'; then
             umount '${ROOTFSDIR}/dev/shm'
-        mountpoint -q '${ROOTFSDIR}/dev' && \
+        fi
+        if mountpoint -q '${ROOTFSDIR}/dev'; then
             umount '${ROOTFSDIR}/dev'
-        mountpoint -q '${ROOTFSDIR}/proc' && \
+        fi
+        if mountpoint -q '${ROOTFSDIR}/proc'; then
             umount '${ROOTFSDIR}/proc'
-        mountpoint -q '${ROOTFSDIR}/sys' && \
+        fi
+        if mountpoint -q '${ROOTFSDIR}/sys'; then
             umount '${ROOTFSDIR}/sys'
+        fi
 
         if [ -e "${ROOTFSDIR}/etc/apt/sources-list" ]; then
             mv "${ROOTFSDIR}/etc/apt/sources-list" \