ilbers/isar

commit 0a7f631f06b6329785e32fd294a51401c6be9d65

wic: Set default WKS_FILE for non efi targets
As riscv64 and mipsel are not supported by grub-efi set other default
WKS_FILE values for them.

Signed-off-by: Anton Mikanovich <amikan@ilbers.de>

meta/classes/imagetypes_wic.bbclass
@@ -8,6 +8,9 @@ USING_WIC = "${@bb.utils.contains('IMAGE_BASETYPES', 'wic', '1', '0', d)}"
 WKS_FILE_CHECKSUM = "${@'${WKS_FULL_PATH}:%s' % os.path.exists('${WKS_FULL_PATH}') if bb.utils.to_boolean(d.getVar('USING_WIC')) else ''}"
 
 WKS_FILE ??= "sdimage-efi"
+# grub-efi is incompatible with targets riscv64 and mipsel
+WKS_FILE:riscv64 ??= "qemuriscv"
+WKS_FILE:mipsel ??= "qemudefault"
 
 do_copy_wks_template[file-checksums] += "${WKS_FILE_CHECKSUM}"
 do_copy_wks_template[vardepsexclude] += "WKS_TEMPLATE_PATH"

meta-isar/scripts/lib/wic/canned-wks/qemudefault.wks
@@ -1,0 +1,5 @@
+# short-description: Create qcow2 image for QEMU machines
+
+part / --source rootfs --fstype=ext4 --label root --align 4096 --size 4G
+
+bootloader

commit 6e31113005ea4b54eade8a1bbde7ba7a7274574c

imagetypes_wic: Fix riscv64 and mipsel usage
We must no set WKS_FILE via overrides in a central file as it will take
precedence over regular assignments that users rightfully expect to
work.

Fixes: 0a7f631f06b6 ("wic: Set default WKS_FILE for non efi targets")
Reported-by: Quirin Gylstorff <quirin.gylstorff@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta-isar/conf/machine/qemumipsel.conf
@@ -8,6 +8,9 @@ KERNEL_NAME ?= "4kc-malta"
 IMAGE_FSTYPES ?= "ext4"
 ROOTFS_EXTRA ?= "128"
 
+# grub-efi is incompatible with targets mipsel, thus no sdimage-efi default
+WKS_FILE ?= "qemudefault"
+
 QEMU_ROOTFS_DEV ?= "sda"
 
 QEMU_ARCH ?= "mipsel"

meta-isar/conf/machine/qemuriscv64.conf
@@ -11,6 +11,9 @@ KERNEL_NAME ?= "riscv64"
 IMAGE_FSTYPES ?= "ext4"
 ROOTFS_EXTRA ?= "128"
 
+# grub-efi is incompatible with targets riscv64, thus no sdimage-efi default
+WKS_FILE ?= "qemuriscv"
+
 QEMU_ROOTFS_DEV ?= "vda"
 
 QEMU_ARCH ?= "riscv64"

meta/classes/imagetypes_wic.bbclass
@@ -8,9 +8,6 @@ USING_WIC = "${@bb.utils.contains('IMAGE_BASETYPES', 'wic', '1', '0', d)}"
 WKS_FILE_CHECKSUM = "${@'${WKS_FULL_PATH}:%s' % os.path.exists('${WKS_FULL_PATH}') if bb.utils.to_boolean(d.getVar('USING_WIC')) else ''}"
 
 WKS_FILE ??= "sdimage-efi"
-# grub-efi is incompatible with targets riscv64 and mipsel
-WKS_FILE:riscv64 ??= "qemuriscv"
-WKS_FILE:mipsel ??= "qemudefault"
 
 do_copy_wks_template[file-checksums] += "${WKS_FILE_CHECKSUM}"
 do_copy_wks_template[vardepsexclude] += "WKS_TEMPLATE_PATH"