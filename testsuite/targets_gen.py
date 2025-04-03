f1b7e26092a043669ac08bc006648336ec18dc04
Anton Mikanovich <amikan@ilbers.de>
2025-04-03 10:17:50 +0200

    CI: Add make world test case
    Add test case which builds all available targets.
    
    Single bitbake usage (directly from mc.conf):
    $ avocado run testsuite/citest.py -t world
    
    Multiple bitbakes usage (from generated yaml file):
    $ avocado run testsuite/citest.py -t world -m testsuite/data/targets.yml
    
    Or the same with ci_build.sh:
    
    There is also a script to regenerate yaml from mc.conf:
    $ ./testsuite/utils/targets_gen.py
    
    This test case requires avocado-framework-plugin-varianter-yaml-to-mux.
    
    Signed-off-by: Anton Mikanovich <amikan@ilbers.de>

---
+++ testsuite/targets_gen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+# This software is a part of Isar.
+# Copyright (C) 2025 ilbers GmbH
+
+# targets_gen.py: Generates yaml for yaml-to-mux Avocado varianter plugin.
+"""
+
+from utils import CIUtils
+
+if __name__ == "__main__":
+    CIUtils.gen_targets_yaml()
