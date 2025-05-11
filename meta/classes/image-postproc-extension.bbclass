ilbers/isar

commit 78b24427a0506e9be253b0bddd9b54c539cf54b3

classes/image-postproc: Add image version
Add the image version as additional identifier to /etc/os-release.
This allows in a update scenario an easier identification of the
the currently used image.

Signed-off-by: Quirin Gylstorff <quirin.gylstorff@siemens.com>

meta/classes/image-postproc-extension.bbclass
@@ -4,10 +4,12 @@
 update_etc_os_release() {
     OS_RELEASE_BUILD_ID=""
     OS_RELEASE_VARIANT=""
+    OS_RELEASE_VARIANT_VERSION=""
     while true; do
         case "$1" in
         --build-id) OS_RELEASE_BUILD_ID=$2; shift ;;
         --variant) OS_RELEASE_VARIANT=$2; shift ;;
+        --version) OS_RELEASE_VARIANT_VERSION=$2; shift ;;
         -*) bbfatal "$0: invalid option specified: $1" ;;
         *) break ;;
         esac
@@ -22,6 +24,11 @@ update_etc_os_release() {
     if [ -n "${OS_RELEASE_VARIANT}" ]; then
         sudo sed -i '/^VARIANT=.*/d' '${IMAGE_ROOTFS}/etc/os-release'
         echo "VARIANT=\"${OS_RELEASE_VARIANT}\"" | \
+            sudo tee -a '${IMAGE_ROOTFS}/etc/os-release'
+    fi
+    if [ -n "${OS_RELEASE_VARIANT_VERSION}" ]; then
+        sudo sed -i '/^ISAR_IMAGE_VERSION=.*/d' '${IMAGE_ROOTFS}/etc/os-release'
+        echo "VARIANT_VERSION=\"${PV}\"" | \
             sudo tee -a '${IMAGE_ROOTFS}/etc/os-release'
     fi
 }
@@ -43,7 +50,7 @@ ROOTFS_POSTPROCESS_COMMAND =+ "image_postprocess_mark"
 image_postprocess_mark() {
     BUILD_ID=$(get_build_id)
     update_etc_os_release \
-        --build-id "${BUILD_ID}" --variant "${DESCRIPTION}"
+        --build-id "${BUILD_ID}" --variant "${DESCRIPTION}" --version "${PV}"
 }
 
 ROOTFS_POSTPROCESS_COMMAND =+ "image_postprocess_machine_id"

commit bfc54360b779b33cf9a02b38607f6d3a8375e6d4

classes/image-postproc: Fix unchangeable VARIANT_VERSION value
During custom /etc/os-release generation in update_etc_os_release()
VARIANT_VERSION was set to PV without taking in account --version option
provided from outside.

Fixes: 78b24427a050 ("classes/image-postproc: Add image version")

Signed-off-by: Anton Mikanovich <amikan@ilbers.de>

meta/classes/image-postproc-extension.bbclass
@@ -28,7 +28,7 @@ update_etc_os_release() {
     fi
     if [ -n "${OS_RELEASE_VARIANT_VERSION}" ]; then
         sudo sed -i '/^VARIANT_VERSION=.*/d' '${IMAGE_ROOTFS}/etc/os-release'
-        echo "VARIANT_VERSION=\"${PV}\"" | \
+        echo "VARIANT_VERSION=\"${OS_RELEASE_VARIANT_VERSION}\"" | \
             sudo tee -a '${IMAGE_ROOTFS}/etc/os-release'
     fi
 }