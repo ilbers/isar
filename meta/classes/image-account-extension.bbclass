ilbers/isar

commit 6144daf9814e7e8c612c2cefe9595fd42bd6d5bc

image-account-extension: Avoid deprecated crypt module
crypt will be dropped from Python 13. Switch to 'openssl passwd' to
avoid extra dependencies like python3-passlib.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/classes/image-account-extension.bbclass
@@ -72,7 +72,6 @@ def image_create_users(d: "DataSmart") -> None:
         None
     """
     import hashlib
-    import crypt
 
     entries = (d.getVar("USERS") or "").split()
     rootfsdir = d.getVar("ROOTFSDIR")
@@ -137,7 +136,7 @@ def image_create_users(d: "DataSmart") -> None:
                 if source_date_epoch:
                     command.append("-e")
                     salt = hashlib.sha256("{}\n".format(source_date_epoch).encode()).hexdigest()[0:15]
-                    password = crypt.crypt(password, "$6${}".format(salt))
+                    password = bb.process.run('openssl passwd -6 --salt {} {}'.format(salt, password))[0].strip()
 
             else:
                 command.append("-e")

commit e8fcfe6fba9a02b60526201e3847e72af2597072

image-accounts: directly pass arguments to openssl instead of shell
When hashing the password, the whole openssl command was passed as a
shell string instead of directly passing the individual arguments as-is.
Further, the arguments were not shell escaped. By that, passwords
containing a string were split into two individual arguments, breaking
the command (or silently set a different password if the remainder
itself was a valid argument).

We fix this by passing the arguments as-is (as list) to bb.process.run.

Fixes: 6144daf9 ("image-account-extension: Avoid deprecated crypt...")
Reported-by: Clara Kowalsky <clara.kowalsky@siemens.com>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/classes/image-account-extension.bbclass
@@ -132,7 +132,7 @@ def image_create_users(d: "DataSmart") -> None:
                 source_date_epoch = d.getVar("SOURCE_DATE_EPOCH")
                 command.append("-e")
                 salt = hashlib.sha256("{}\n".format(source_date_epoch).encode()).hexdigest()[0:15]
-                password = bb.process.run('openssl passwd -6 --salt {} {}'.format(salt, password))[0].strip()
+                password = bb.process.run(['openssl', 'passwd', '-6', '--salt', salt, password])[0].strip()
 
             else:
                 command.append("-e")