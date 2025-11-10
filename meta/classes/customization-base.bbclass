ilbers/isar

commit 9a2919be787178d10180dc940273897e2b3bab62

classes: add 'customization' package support
A number of downstream layers have customization dpkg-raw recipes making use of
template variables to support build-time settings: add the infrastructure to
Isar to help uniformization of such recipes. Settings are often specific to a
DISTRO, MACHINE or IMAGE: provide a mechanism to decorate (prefix or suffix)
packages being constructed and pulled into the image. This helps use of
customization packages in multiconfig builds but also with the deployment of
binary package feeds for projects supporting multiple variants.

To create a customization package, a recipe must inherit the
`dpkg-customization` bbclass. By default, images will inherit the
`image-customizations` class, which will install the package for any
customizations listed in `CUSTOMIZATIONS`.

To add variables to the customization package names, beyond the default DISTRO
and MACHINE, add them to `CUSTOMIZATION_VARS` as variable references
(`${DISTRO}`, `${MACHINE}`). By default, these variables are appended to the
package name, but if they should be prefixed, add them to
`CUSTOMIZATION_VARS_PREFIXED`. By default, the `DISTRO` and `MACHINE` variables
are used, and the `DISTRO` is prefixed, while the `MACHINE` is suffixed.

To enable support for per-image changes, and per-image customization packages,
add the image name to `CUSTOMIZATION_FOR_IMAGES`. In the customization package
recipe, the `IMAGE` variable will be set to the image name, and the package name
will be decorated with the image name. The `IMAGE` will also be added to the
`OVERRIDES`, allowing for variable definitions like this in the customization
recipe:

    HOSTNAME:isar-image-ci = "isar-ci"

When `CUSTOMIZATION_FOR_IMAGES` is defined, `${IMAGE}` will be implicitly added
to `CUSTOMIZATION_VARS`, and the package will be decorated with the image name.

Signed-off-by: Christopher Larson <chris.larson@siemens.com>

meta/classes/customization-base.bbclass
@@ -1,0 +1,21 @@
+# Common class for customization packages, used by dpkg-customization.bbclass
+# and image-customizations.bbclass.
+
+LIST_VARIABLES += "CUSTOMIZATIONS CUSTOMIZATION_VARS CUSTOMIZATION_VARS_PREFIXED"
+
+CUSTOMIZATIONS ?= ""
+CUSTOMIZATIONS[doc] = "List of customization packages to be installed in images."
+
+CUSTOMIZATION_VARS ?= "${DISTRO} ${MACHINE}"
+CUSTOMIZATION_VARS[doc] = "List of variables that should be added to customization package names."
+CUSTOMIZATION_VARS_IMAGE ?= "${IMAGE}"
+CUSTOMIZATION_VARS:append = " ${@d.getVar('CUSTOMIZATION_VARS_IMAGE') if d.getVar('CUSTOMIZATION_FOR_IMAGES').strip() else ''}"
+
+CUSTOMIZATION_VARS_PREFIXED ?= "${DISTRO}"
+CUSTOMIZATION_VARS_PREFIXED[doc] = "List of variables from CUSTOMIZATION_VARS that should be prefixed rather than suffixed to customization package names."
+
+CUSTOMIZATION_FOR_IMAGES ?= ""
+CUSTOMIZATION_FOR_IMAGES[doc] = "List of images that should install the customizations in CUSTOMIZATIONS"
+
+CUSTOMIZATION_PREFIX ?= "${@'-'.join(var for var in d.getVar('CUSTOMIZATION_VARS').split() if var in d.getVar('CUSTOMIZATION_VARS_PREFIXED'))}"
+CUSTOMIZATION_SUFFIX ?= "${@'-'.join(var for var in d.getVar('CUSTOMIZATION_VARS').split() if var not in d.getVar('CUSTOMIZATION_VARS_PREFIXED'))}"

meta/classes/dpkg-customization.bbclass
@@ -1,0 +1,37 @@
+inherit dpkg-raw customization-base
+
+PRE_CUSTOMIZATION_PN := "${PN}"
+FILESEXTRAPATHS:prepend := "${THISDIR}/${BPN}:"
+PN =. "${@d.getVar('CUSTOMIZATION_PREFIX') + '-' if d.getVar('CUSTOMIZATION_PREFIX') else ''}"
+PN .= "${@'-' + d.getVar('CUSTOMIZATION_SUFFIX') if d.getVar('CUSTOMIZATION_SUFFIX') else ''}"
+
+BBCLASSEXTEND = "${@' '.join(f'dpkg-customization:{image}' for image in d.getVar('CUSTOMIZATION_FOR_IMAGES').split())}"
+
+python customization_virtclass_handler() {
+    orig_pn = d.getVar('PRE_CUSTOMIZATION_PN')
+
+    d = e.data
+    extend = d.getVar('BBEXTENDCURR') or ''
+    variant = d.getVar('BBEXTENDVARIANT') or ''
+    if extend != 'dpkg-customization' or variant == '':
+        d.appendVar('PROVIDES', f' {orig_pn}')
+        d.setVar('IMAGE', '')
+        return
+
+    vars = (d.getVar('CUSTOMIZATION_VARS', expand=False) or '').split()
+    if '${IMAGE}' not in vars:
+        return
+
+    images = (d.getVar('CUSTOMIZATION_FOR_IMAGES') or '').split()
+    if variant not in images:
+        return
+
+    d.setVar('IMAGE', variant)
+    if not d.getVar('BPN').endswith(f'-{variant}'):
+        d.appendVar('BPN', f'-{variant}')
+    d.appendVar('PROVIDES', f' {orig_pn}-{variant}')
+    d.appendVar('OVERRIDES', f':{variant}')
+}
+addhandler customization_virtclass_handler
+customization_virtclass_handler[eventmask] = "bb.event.RecipePreFinalise"
+

meta/classes/image-customizations.bbclass
@@ -1,0 +1,30 @@
+inherit customization-base
+
+def customization_packages(d):
+    customizations = d.getVar('CUSTOMIZATIONS').split()
+    if not customizations:
+        return ''
+
+    # Use image-specific customization if enabled for this image
+    images = (d.getVar('CUSTOMIZATION_FOR_IMAGES') or '').split()
+    image = d.getVar('BPN')
+    if not images or image not in images:
+        d.setVar('IMAGE', '')
+    else:
+        d.setVar('IMAGE', image)
+
+    prefix = d.getVar('CUSTOMIZATION_PREFIX')
+    if prefix:
+        prefix += '-'
+
+    suffix = d.getVar('CUSTOMIZATION_SUFFIX')
+    if suffix:
+        suffix = '-customization-' + suffix
+    else:
+        suffix = '-customization'
+
+    customizations = [ prefix + package + suffix for package in customizations ]
+    return ' '.join(customizations)
+
+CUSTOMIZATION_PACKAGES = "${@ customization_packages(d) }"
+IMAGE_INSTALL:append = " ${CUSTOMIZATION_PACKAGES}"

meta/classes/image.bbclass
@@ -80,6 +80,7 @@ inherit image-tools-extension
 inherit image-postproc-extension
 inherit image-locales-extension
 inherit image-account-extension
+inherit image-customizations
 
 # Extra space for rootfs in MB
 ROOTFS_EXTRA ?= "64"

commit 5bb743ad0ae4e87ef5a14ce4897c14694b46d0c5

customizations: defer IMAGE variable expansion in CUSTOMIZATION_VARS
The previous logic for conditionally adding the IMAGE variable to
CUSTOMIZATION_VARS suffered from a Bitbake variable expansion
timing issue.

The original line `CUSTOMIZATION_VARS_IMAGE ?= "${IMAGE}"` would evaluate
the `${IMAGE}` variable too early in the parsing process. If `IMAGE` had
not yet been fully defined or resolved to its final value (e.g.,
`isar-image-base`), `CUSTOMIZATION_VARS_IMAGE` would incorrectly store
the literal string `"${IMAGE}"`.

Consequently, when `CUSTOMIZATION_VARS` was later appended using a
Python expression that retrieved `CUSTOMIZATION_VARS_IMAGE`, it would
receive this unexpanded literal string. This led to build failures and
errors such as:

  ERROR: ... Unable to get checksum for ... ${IMAGE} SRC_URI entry ...

Introduce a new Bitbake override, `:customization-for-images`, which
is conditionally activated when `CUSTOMIZATION_FOR_IMAGES` is set (to
preserve the initial semantic of 9a2919be). The `IMAGE` variable is
then appended to `CUSTOMIZATION_VARS` specifically within this override
context:

  `CUSTOMIZATION_VARS:append:customization-for-images = " ${IMAGE}"`

By associating the append operation with an override, the expansion is
deferred and ensures that `${IMAGE}` is properly resolved to its value
before being added to `CUSTOMIZATION_VARS`.

Fixes: 9a2919be
Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

meta/classes/customization-base.bbclass
@@ -8,8 +8,8 @@ CUSTOMIZATIONS[doc] = "List of customization packages to be installed in images.
 
 CUSTOMIZATION_VARS ?= "${DISTRO} ${MACHINE}"
 CUSTOMIZATION_VARS[doc] = "List of variables that should be added to customization package names."
-CUSTOMIZATION_VARS_IMAGE ?= "${IMAGE}"
-CUSTOMIZATION_VARS:append = " ${@d.getVar('CUSTOMIZATION_VARS_IMAGE') if d.getVar('CUSTOMIZATION_FOR_IMAGES').strip() else ''}"
+OVERRIDES:append = "${@ ':customization-for-images' if (d.getVar('CUSTOMIZATION_FOR_IMAGES') or '').strip() else '' }"
+CUSTOMIZATION_VARS:append:customization-for-images = " ${IMAGE}"
 
 CUSTOMIZATION_VARS_PREFIXED ?= "${DISTRO}"
 CUSTOMIZATION_VARS_PREFIXED[doc] = "List of variables from CUSTOMIZATION_VARS that should be prefixed rather than suffixed to customization package names."