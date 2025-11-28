ilbers/isar

commit 3896e4daafd0db82731707f692a8ea3a2982445a

initramfs-tee-ftpm-hook: Use override to model variations
Less code, specifically when adding more bits under
TEE_SUPPLICANT_IN_USERLAND control.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/recipes-initramfs/initramfs-tee-ftpm-hook/initramfs-tee-ftpm-hook_0.3.bb
@@ -14,6 +14,8 @@ SRC_URI += "file://local-top"
 # NOTE: default will eventually be flipped
 TEE_SUPPLICANT_IN_USERLAND ?= "1"
 
-HOOK_PREREQ = "${@'tee-supplicant' if bb.utils.to_boolean(d.getVar('TEE_SUPPLICANT_IN_USERLAND')) else ''}"
+OVERRIDES .= "${@'supp-user' if bb.utils.to_boolean(d.getVar('TEE_SUPPLICANT_IN_USERLAND')) else ''}"
+
+HOOK_PREREQ:supp-user = "tee-supplicant"
 HOOK_ADD_MODULES = "amdtee arm-tstee optee qcomtee tpm_ftpm_tee"
-SCRIPT_PREREQ = "${@'tee-supplicant' if bb.utils.to_boolean(d.getVar('TEE_SUPPLICANT_IN_USERLAND')) else ''}"
+SCRIPT_PREREQ:supp-user = "tee-supplicant"

commit 9bb412e43b9d4275b7639430499bf50953b5df12

initramfs-tee-ftpm-hook: Fix missing separator in OVERRIDES append
Prepend the missing colon to the appended string to ensure 'supp-user'
is registered as a distinct override.

This ensures that SRC_URI:supp-user takes effect when
TEE_SUPPLICANT_IN_USERLAND is to "1".

Fixes: 3896e4da ("initramfs-tee-ftpm-hook: Use override to model variations")
Signed-off-by: Sai Sree Kartheek Adivi <s-adivi@ti.com>

meta/recipes-initramfs/initramfs-tee-ftpm-hook/initramfs-tee-ftpm-hook_0.3.bb
@@ -14,7 +14,7 @@ SRC_URI += "file://local-top"
 # NOTE: default will eventually be flipped
 TEE_SUPPLICANT_IN_USERLAND ?= "1"
 
-OVERRIDES .= "${@'supp-user' if bb.utils.to_boolean(d.getVar('TEE_SUPPLICANT_IN_USERLAND')) else ''}"
+OVERRIDES .= "${@':supp-user' if bb.utils.to_boolean(d.getVar('TEE_SUPPLICANT_IN_USERLAND')) else ''}"
 
 HOOK_PREREQ:supp-user = "tee-supplicant"
 HOOK_ADD_MODULES = "amdtee arm-tstee optee qcomtee tpm_ftpm_tee"