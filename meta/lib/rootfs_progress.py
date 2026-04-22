ilbers/isar

commit 938dab363006529f0b5296dfbc09ef306fc8c3d7

Report approximate progress during initrd generation
On non native architectures, the initrd generation has to be emulated.
In combination with kernels that provide many modules (like the distro
kernels), this leads to long initrd build times. To give a rough
estimation of the duration, we add progress reporting to that step.

As we always build the initrd with MODULES=most, we know from
experiments, that ~50% of the kernel modules of the rootfs are included
in the initrd. We use that number as a guesstimate, as we don't have
precise numbers when starting the task.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/lib/rootfs_progress.py
@@ -28,13 +28,15 @@ class PkgsProgressHandler(bb.progress.ProgressHandler):
             self._linebuffer = self._linebuffer[breakpos:]
 
             if self._stage == 'prepare':
-                match = re.search(
-                    r'^([0-9]+) upgraded, ([0-9]+) newly installed', line)
-                if match:
-                    self._num_pkgs = int(match.group(1)) + int(match.group(2))
-                    self._stage = 'post-prepare'
+                self.process_total(line)
             else:
                 self.process_line(line)
+
+    def process_total(self, line):
+        m = re.search(r'^([0-9]+) upgraded, ([0-9]+) newly installed', line)
+        if m:
+            self._num_pkgs = int(m.group(1)) + int(m.group(2))
+            self._stage = 'post-prepare'
 
     def process_line(self, line):
         return
@@ -65,3 +67,24 @@ class PkgsInstallProgressHandler(PkgsProgressHandler):
 
         progress = 99 * (self._pkg + self._pkg_set_up) / (self._num_pkgs * 2)
         self._progress.update(progress)
+
+
+class InitrdProgressHandler(PkgsProgressHandler):
+    def __init__(self, d, outfile, otherargs=None):
+        super().__init__(d, outfile)
+
+    def process_total(self, line):
+        m = re.search(r'^Total number of modules: ([0-9]+)', line)
+        if m:
+            # in MODULES=most mode, we install ~half of all modules
+            self._num_pkgs = int(m.group(1)) // 2
+            self._stage = 'post-prepare'
+
+    def process_line(self, line):
+        if line.startswith('Adding module'):
+            self._pkg += 1
+        elif line.startswith('(excluding'):
+            self._pkg += len(line.split(' ')) - 1
+        else:
+            return
+        self._progress.update(99 * self._pkg / self._num_pkgs)

meta/classes/rootfs.bbclass
@@ -454,6 +454,10 @@ do_generate_initramfs[sstate-outputdirs] = "${DEPLOY_DIR_IMAGE}"
 python do_generate_initramfs() {
     bb.build.exec_func('rootfs_do_mounts', d)
     bb.build.exec_func('rootfs_do_qemu', d)
+
+    progress_reporter = bb.progress.ProgressHandler(d)
+    d.rootfs_progress = progress_reporter
+
     try:
         bb.build.exec_func('rootfs_generate_initramfs', d)
     finally:
@@ -468,7 +472,9 @@ rootfs_generate_initramfs[progress] = "custom:rootfs_progress.InitrdProgressHand
 rootfs_generate_initramfs() {
     if [ -n "$(sudo find '${ROOTFSDIR}/boot' -type f -name 'vmlinu[xz]*')" ]; then
         sudo -E chroot "${ROOTFSDIR}" sh -c '\
+            mods_total="$(find /usr/lib/modules -type f -name '*.ko*' | wc -l)"; \
             export kernel_version=$(basename /boot/vmlinu[xz]-* | cut -d'-' -f2-); \
+            echo "Total number of modules: $mods_total"; \
             echo "Generating initrd for kernel version: $kernel_version"; \
             update-initramfs -u -v -k "$kernel_version";'
         if [ -n "${INITRD_DEPLOY_FILE}" ]; then

commit 1a04e7f209e40acf117188cdcf0f3a95d41b0009

Fix ZeroDivisionError for kernels without modules
Ensure that `num_pkgs` is at least 1 to avoid division by zero.

Fixes: 938dab36 ("Report approximate progress during initrd generation")

Signed-off-by: Stefan Koch <stefan-koch@siemens.com>
Reviewed-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/lib/rootfs_progress.py
@@ -77,7 +77,7 @@ class InitrdProgressHandler(PkgsProgressHandler):
         m = re.search(r'^Total number of modules: ([0-9]+)', line)
         if m:
             # in MODULES=most mode, we install ~half of all modules
-            self._num_pkgs = int(m.group(1)) // 2
+            self._num_pkgs = max(1, int(m.group(1)) // 2)
             self._stage = 'post-prepare'
 
     def process_line(self, line):