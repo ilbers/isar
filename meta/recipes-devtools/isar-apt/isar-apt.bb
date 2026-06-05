ilbers/isar

commit be290896c9fb5b1236f10f18809ca603ec873aa0

isar-apt: support further optional fields in Release
The isar-apt repository is a proper apt repository which can be
distributed as-is. A common use-case for that is to build packages which
are then used in a standard Debian by adding the repository to the apt
sources. In this case, it is desired to fill some of the optional fields
in Release to enable fine-grained pinnings.

We extend the repo_create function to allow appending arbitrary fields
to the Release file. To make that easy to use, we introduce the
ISAR_APT_OPT_FIELD bitbake var map. As the do_cache_config task now
depends on user-input, we also ensure it is rebuild when changed.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/recipes-devtools/isar-apt/isar-apt.bb
@@ -9,6 +9,8 @@ inherit repository
 SRC_URI = "file://distributions.in"
 
 do_cache_config[lockfiles] = "${REPO_ISAR_DIR}/isar.lock"
+do_cache_config[vardeps] += "ISAR_APT_OPT_FIELD"
+do_cache_config[cleandirs] += "${REPO_ISAR_DIR}/${DISTRO}/conf"
 
 # Generate reprepro config for current distro if it doesn't exist. Once it's
 # generated, this task should do nothing.
@@ -16,7 +18,9 @@ do_cache_config() {
     repo_create "${REPO_ISAR_DIR}"/"${DISTRO}" \
         "${REPO_ISAR_DB_DIR}"/"${DISTRO}" \
         "${DEBDISTRONAME}" \
-        "${WORKDIR}/distributions.in"
+        "${WORKDIR}/distributions.in" \
+        "" \
+        "${@ repo_expand_opt_fields(d, 'ISAR_APT_OPT_FIELD')}"
 }
 
 addtask cache_config after do_unpack before do_build

meta/classes/repository.bbclass
@@ -4,12 +4,19 @@
 #
 # SPDX-License-Identifier: MIT
 
+def repo_expand_opt_fields(d, var):
+    f = d.getVarFlags(var)
+    if not f:
+        return ''
+    return '\n'.join('{}: {}'.format(k, v) for k, v in f.items())
+
 repo_create() {
     local dir="$1"
     local dbdir="$2"
     local codename="$3"
     local distros_in="$4"
     local keyfiles="$5"
+    local conf_append="$6"
 
     if [ -n "${GNUPGHOME}" ]; then
         export GNUPGHOME="${GNUPGHOME}"
@@ -26,6 +33,9 @@ repo_create() {
 	      option="${option}${keyid} "
 	    done
 	    echo "SignWith: ${option}" >> "${dir}"/conf/distributions
+        fi
+        if [ -n "${conf_append}" ]; then
+            echo "${conf_append}" >> "${dir}"/conf/distributions
         fi
     fi
     if [ ! -d "${dbdir}" ]; then

commit 4741b0d5da37e75f1995f7f0820e15b2263ca7e2

isar-apt: fix race-condition in do_cache_config
Repo configuration operations on the isar apt have to happen while
holding the isar lock. However, bitbake executes the cleandirs operation
without holding the lock, introducing a race-condition that sporadically
breaks the build with the following error:

  Error opening config file '/isar/build/tmp/deploy/isar-apt/
  debian-trixie/apt/debian-trixie/conf/distributions': No such file
  or directory(2)

We fix this by open-coding the cleandirs logic inside the function and
by that under the lock.

Fixes: be290896 ("isar-apt: support further optional fields ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/recipes-devtools/isar-apt/isar-apt.bb
@@ -10,11 +10,15 @@ SRC_URI = "file://distributions.in"
 
 do_cache_config[lockfiles] = "${REPO_ISAR_DIR}/isar.lock"
 do_cache_config[vardeps] += "ISAR_APT_OPT_FIELD"
-do_cache_config[cleandirs] += "${REPO_ISAR_DIR}/${DISTRO}/conf"
 
 # Generate reprepro config for current distro if it doesn't exist. Once it's
 # generated, this task should do nothing.
 do_cache_config() {
+    # this part must be executed while holding the isar.lock, hence do not move
+    # it to cleandirs (these are executed without holding the lock)
+    rm -rf ${REPO_ISAR_DIR}/${DISTRO}/conf
+    mkdir -p ${REPO_ISAR_DIR}/${DISTRO}/conf
+
     repo_create "${REPO_ISAR_DIR}"/"${DISTRO}" \
         "${REPO_ISAR_DB_DIR}"/"${DISTRO}" \
         "${DEBDISTRONAME}" \