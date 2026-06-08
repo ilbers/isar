ilbers/isar

commit 367c5d993498b4aa27313bcb890e2d7c608821f3

linux-distro: Fix multiple provider warnings
As now all recipe variants of this stub target provide the same, we get
warnings like these when using the distro kernel:

NOTE: Multiple providers are available for linux-image-arm64 (linux-distro, linux-distro-native)
Consider defining a PREFERRED_PROVIDER entry to match linux-image-arm64

We can resolve that by limiting the PROVIDES additions to the base
target again as all of them are stubs anyway, Debian will provide the
actual packages.

Fixes: b7e3c78766c3 ("module.inc: fix kbuild dependency")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/recipes-kernel/linux/linux-distro.bb
@@ -6,6 +6,9 @@
 # SPDX-License-Identifier: MIT
 
 python() {
+    if ("class-native" or "class-compat") in d.getVar("OVERRIDES").split(":"):
+        return
+
     distro_kernels = d.getVar('DISTRO_KERNELS') or ""
     for kernel in distro_kernels.split():
         d.appendVar('PROVIDES', ' linux-image-' + kernel)

commit bc6e0b963733135ffe57f46c0c08d12a2bd3cf9a

linux-distro: Fix check for native or compat override
We must not inject the stubs on compat or native, as otherwise we get
the multiple providers warning. Commit 367c5d99 tried to fix this, but
used incorrect Python syntax to express the condition. By that, only the
class-native was excluded, but not compat.

Fixes: 367c5d99 ("linux-distro: Fix multiple provider warnings")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Reviewed-by: Jan Kiszka <jan.kiszka@siemens.com>

meta/recipes-kernel/linux/linux-distro.bb
@@ -6,7 +6,8 @@
 # SPDX-License-Identifier: MIT
 
 python() {
-    if ("class-native" or "class-compat") in d.getVar("OVERRIDES").split(":"):
+    overrides = d.getVar("OVERRIDES").split(":")
+    if any(o in overrides for o in ("class-native", "class-compat")):
         return
 
     distro_kernels = d.getVar('DISTRO_KERNELS') or ""