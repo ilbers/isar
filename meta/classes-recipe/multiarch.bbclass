ilbers/isar

commit 327fb3133a71b0a1d25b4678d0da1cf6e0647dd4

sbuild: do not build arch all packages on cross
Architecture all packages often cannot be built in cross mode, as the
dependencies cannot be resolved in the host architecture. This
especially applies to all packages which split their build dependencies
into Build-Depends-Arch and Build-Depends-Indep. We already have logic
in isar to built DPKG_ARCH=all packages non-cross, but this does not
work for packages that generate both arch specific and arch all binary
packages.

While sbuild automatically disables builds of arch-all packages on
cross, we previously explicitly overrode this setting. We now change
this by explicitly setting --no-arch-all flag on cross and --arch-all on
non-cross.

To bitbake depend on arch=all packages, we introduce an interface
similar to the -native: For mixed-architecture packages, the user needs
to extend the PROVIDES by adding -archall to the arch=all packages. This
internally is then dispatched to the -native target, not requiring any
changes on the consumer side (i.e. DEPENDS does not need to add the
-archall).

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/classes-recipe/multiarch.bbclass
@@ -83,6 +83,10 @@ python multiarch_virtclass_handler() {
             for v in val.split():
                 if v.endswith('-compat') or v.endswith('-native'):
                     multiarch_var.append(v)
+                # dispatch -archall (arch=all) to native variant
+                if v.endswith('-archall'):
+                    if suffix == '-native':
+                        multiarch_var.append(v[:-len('-archall')])
                 else:
                     multiarch_var.append(v + suffix)
             d.setVar(var, ' '.join(multiarch_var))

meta/classes-recipe/dpkg.bbclass
@@ -109,8 +109,9 @@ dpkg_runbuild() {
 
     DSC_FILE=$(find ${WORKDIR} -maxdepth 1 -name "${DEBIAN_SOURCE}_*.dsc" -print)
 
-    sbuild -A -n -c ${SBUILD_CHROOT} --chroot-mode=schroot \
+    sbuild -n -c ${SBUILD_CHROOT} --chroot-mode=schroot \
         --host=${PACKAGE_ARCH} --build=${BUILD_ARCH} ${profiles} \
+        ${@'--no-arch-all' if 'cross' in isar_deb_build_profiles(d).split() else '--arch-all'} \
         --no-run-lintian --no-run-piuparts --no-run-autopkgtest --resolve-alternatives \
         --bd-uninstallable-explainer=apt \
         --no-apt-update --apt-distupgrade \

commit ae7633dc5ab0d3b2589bdbc72d5701cb595d8cc2

fix: redirect archall dependency to native provider on non cross builds
The extend_provides needs to mirror the -archall pattern from fixup_depends:
when a provide ends in -archall, the arch-independent (native-equivalent)
provider must be exposed under its bare name, not <name>-archall-native.

We change it so that for the native case it strips -archall and emits
the bare name

Fixes: 327fb313 ("sbuild: do not build arch all packages on cross")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta/classes-recipe/multiarch.bbclass
@@ -16,7 +16,11 @@ python() {
         if not pn_multiarch_target(pn):
             all_provides = (d.getVar('PROVIDES') or '').split()
             for p in all_provides:
-                if not pn_multiarch_target(p):
+                if p.endswith('-archall'):
+                    # arch=all provider: expose bare name for the native-equivalent
+                    if provides == 'native':
+                        d.appendVar('PROVIDES', ' ' + p[:-len('-archall')])
+                elif not pn_multiarch_target(p):
                     d.appendVar('PROVIDES', f' {p}-{provides}')
             d.appendVar('PROVIDES', f' {pn}-{provides}')
 