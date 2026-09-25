8a2bd3fb1d02d4f3aba36c7064c39e49977e4704
Felix Moessbauer <felix.moessbauer@siemens.com>
2026-09-25 09:30:36 +0200

    dracut-module: make module-setup compatible with sysroot
    As a preparation for cross-building dracut initrds, we need to ensure
    the module setup does not call external tooling (which would have to be
    emulated). Further, all tool output must be consumable under sysroot as
    well (e.g. paths must be resolved in the target chroot). We fix this by
    replacing calls to tooling with the corresponding dracut helpers.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- meta-isar/recipes-initramfs/dracut-example-lighttpd/files/install.sh
+++ meta-isar/recipes-initramfs/dracut-example-lighttpd/files/install.sh
@@ -1,5 +1,9 @@
-inst_multiple -o /usr/lib/lighttpd/*.so
-inst_multiple -o /usr/share/lighttpd/*
+# Copy optional lighttpd modules and assets from the target sysroot.
+for source in "${dracutsysrootdir}"/usr/lib/*/lighttpd/*.so \
+			  "${dracutsysrootdir}"/usr/share/lighttpd/*; do
+	[[ -e "$source" ]] || continue
+	inst_simple "$source" "${source#"$dracutsysrootdir"}"
+done
 
 inst_simple "${moddir}/lighttpd.service" "$systemdsystemunitdir/lighttpd.service"
 inst_simple "${moddir}/lighttpd.conf" /etc/lighttpd/lighttpd.conf
@@ -13,8 +17,8 @@ mkdir -p -m 0700 "$initdir/var/cache/lighttpd/uploads"
 mkdir -p -m 0700 "$initdir/var/log/lighttpd/"
 mkdir -p -m 0755 "$initdir/var/www/html"
 
-/usr/bin/install -m 0644 /usr/share/lighttpd/index.html "$initdir/var/www/html/index.html"
+inst_simple "${dracutsysrootdir}"/usr/share/lighttpd/index.html /var/www/html/index.html
 touch "$moddir"/error.log
-/usr/bin/install -m 0644 "$moddir"/error.log "$initdir/var/log/lighttpd/error.log"
+inst_simple "$moddir"/error.log /var/log/lighttpd/error.log
 chown -R www-data:www-data "$initdir/var/log/lighttpd/"
 systemctl -q --root "$initdir" enable lighttpd
